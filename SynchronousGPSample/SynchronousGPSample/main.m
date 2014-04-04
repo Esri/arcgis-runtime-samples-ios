8bf860b849e4662326a8205aa9baa7d919998ac8
Gagandeep Singh <gagan.singh04@gmail.com>
2014-04-04 09:16:47 -0700

    Ported to SynchronousGPSample to Storyboard

--- SynchronousGPSample/SynchronousGPSample/main.m
+++ SynchronousGPSample/SynchronousGPSample/main.m
@@ -11,11 +11,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "SynchronousGPSampleAppDelegate.h"
 
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        int retVal = UIApplicationMain(argc, argv, nil, nil);
+        int retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([SynchronousGPSampleAppDelegate class]));
         return retVal;
     }
 }

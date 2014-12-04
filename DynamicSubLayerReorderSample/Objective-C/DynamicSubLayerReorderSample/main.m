52ab179f0a4c9e319fad7495f60b03ce70595f40
Gagandeep Singh <gagan.singh04@gmail.com>
2014-12-04 09:06:15 -0800

    Created a new sample in ObjC for reordering the sub layers in a dynamic map service

---
+++ DynamicSubLayerReorderSample/Objective-C/DynamicSubLayerReorderSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReorderLayersSample
+//
+//  Created by Gagandeep Singh on 11/18/14.
+//  Copyright (c) 2014 Esri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

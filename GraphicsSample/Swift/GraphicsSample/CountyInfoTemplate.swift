82a49f0e54230baa94473b778f834d4608f8c7d6
Gagandeep Singh <gagan.singh04@gmail.com>
2014-12-10 08:54:20 -0800

    updated the copyright year

--- GraphicsSample/Swift/GraphicsSample/CountyInfoTemplate.swift
+++ GraphicsSample/Swift/GraphicsSample/CountyInfoTemplate.swift
@@ -1,5 +1,5 @@
 //
-// Copyright 2012 ESRI
+// Copyright 2014 ESRI
 //
 // All rights reserved under the copyright laws of the United States
 // and applicable international laws, treaties, and conventions.

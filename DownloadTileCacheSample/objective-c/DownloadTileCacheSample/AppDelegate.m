15c55163e3f93449491cd557419797196ad75ca4
Gagandeep Singh <gagan.singh04@gmail.com>
2015-03-27 12:07:30 -0700

    conditionally checking if iOS SDK is 8.0 or later for UIUserNotificationSettings

--- DownloadTileCacheSample/objective-c/DownloadTileCacheSample/AppDelegate.m
+++ DownloadTileCacheSample/objective-c/DownloadTileCacheSample/AppDelegate.m
@@ -28,11 +28,14 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 80000
     //request permission for local notifications in iOS 8
     if ([UIApplication instancesRespondToSelector:@selector(registerUserNotificationSettings:)]){
         [application registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert|UIUserNotificationTypeBadge|UIUserNotificationTypeSound categories:nil]];
     }
+#endif
     return YES;
+
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application

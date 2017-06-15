3843ea027b3393ad1cbc1d2c80bd82a5c4c6aec6
ikbalyasarGis <miyasar@gtu.edu.tr>
2017-06-15 14:33:37 +0300

    Custom callout sample added.

---
+++ arcgis-ios-sdk-samples/CustomCalloutViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CustomCalloutViewController.swift
+//  arcgis-ios-sdk-samples
+//
+//  Created by İkbal Yaşar on 6/15/17.
+//  Copyright © 2017 Esri. All rights reserved.
+//
+
+import UIKit
+
+class CustomCalloutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

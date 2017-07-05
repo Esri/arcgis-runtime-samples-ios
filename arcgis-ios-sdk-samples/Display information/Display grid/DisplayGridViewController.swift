bedf0c55c29fac52fe6940c253322986acce4c6a
Nimesh Jarecha <njarecha@esri.com>
2017-07-05 11:12:09 -0700

    added Readme and code cleanup

--- arcgis-ios-sdk-samples/Display information/Display grid/DisplayGridViewController.swift
+++ arcgis-ios-sdk-samples/Display information/Display grid/DisplayGridViewController.swift
@@ -18,16 +18,14 @@ import ArcGIS
 
 class DisplayGridViewController: UIViewController, UIAdaptivePresentationControllerDelegate {
 
-    @IBOutlet var mapView: AGSMapView!
-    @IBOutlet var settingsContainerView: UIView!
-    
+    @IBOutlet weak var mapView: AGSMapView!
     private var gridSettingsViewController:DisplayGridSettingsViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Add the source code button item to the right of navigation bar
-        (self.navigationItem.rightBarButtonItem as! SourceCodeBarButtonItem).filenames = ["DisplayGridViewController", "DisplayGridSettingsViewController"]
+        (navigationItem.rightBarButtonItem as! SourceCodeBarButtonItem).filenames = ["DisplayGridViewController", "DisplayGridSettingsViewController"]
 
         // Initialize map with imagery basemap
         let map = AGSMap(basemap: AGSBasemap.imagery())
@@ -37,15 +35,10 @@ class DisplayGridViewController: UIViewController, UIAdaptivePresentationControl
         map.initialViewpoint = AGSViewpoint(center: center, scale: 23227)
         
         // Assign map to the map view
-        self.mapView.map = map
+        mapView.map = map
         
         // Add lat long grid
-        self.mapView.grid = AGSLatitudeLongitudeGrid()
-
-        self.mapView.viewpointChangedHandler = {
-            print("\(self.mapView.visibleArea?.extent.center)")
-            print("\(self.mapView.mapScale)")
-        }
+        mapView.grid = AGSLatitudeLongitudeGrid()
     }
     
     override func didReceiveMemoryWarning() {
@@ -67,19 +60,19 @@ class DisplayGridViewController: UIViewController, UIAdaptivePresentationControl
         if segue.identifier == "DisplayGridSettingsSegue" {
             //
             // Set grid settings view controller
-            self.gridSettingsViewController = segue.destination as! DisplayGridSettingsViewController
-            self.gridSettingsViewController.mapView = self.mapView
+            gridSettingsViewController = segue.destination as! DisplayGridSettingsViewController
+            gridSettingsViewController.mapView = self.mapView
 
             // Pop over settings
-            self.gridSettingsViewController.presentationController?.delegate = self
-            self.gridSettingsViewController.popoverPresentationController?.passthroughViews = [self.mapView]
+            gridSettingsViewController.presentationController?.delegate = self
+            gridSettingsViewController.popoverPresentationController?.passthroughViews = [self.mapView]
             
             // Preferred content size
-            if self.traitCollection.horizontalSizeClass == .regular && self.traitCollection.verticalSizeClass == .regular {
-                self.gridSettingsViewController.preferredContentSize = CGSize(width: 375, height: 350)
+            if traitCollection.horizontalSizeClass == .regular && traitCollection.verticalSizeClass == .regular {
+                gridSettingsViewController.preferredContentSize = CGSize(width: 375, height: 350)
             }
             else {
-                self.gridSettingsViewController.preferredContentSize = CGSize(width: 375, height: 250)
+                gridSettingsViewController.preferredContentSize = CGSize(width: 375, height: 250)
             }
         }
     }

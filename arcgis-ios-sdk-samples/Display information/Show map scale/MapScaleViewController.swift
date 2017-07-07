daca155d79cd4203d8b5e98f89795d3d09166555
fozoglu <f.ozoglu@gmail.com>
2017-07-07 09:36:56 +0300

    Added MapScaleListViewController
    You can choose one of the scales in the list.

--- arcgis-ios-sdk-samples/Display information/Show map scale/MapScaleViewController.swift
+++ arcgis-ios-sdk-samples/Display information/Show map scale/MapScaleViewController.swift
@@ -15,22 +15,24 @@
 import UIKit
 import ArcGIS
 
-class MapScaleViewController: UIViewController {
-
+class MapScaleViewController: UIViewController, MapScaleListVCDelegate, UIAdaptivePresentationControllerDelegate {
+    
     @IBOutlet private weak var mapView:AGSMapView!
     @IBOutlet private weak var scaleView: UIView!
     @IBOutlet private weak var scaleLabel: UILabel!
     
     var pinch : UIPinchGestureRecognizer!
     
+    let scales = ["1/1000", "1/5000", "1/25000", "1/50000", "1/100000", "1/1000000", "1/5000000"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         //add the source code button item to the right of navigation bar
         (self.navigationItem.rightBarButtonItem as! SourceCodeBarButtonItem).filenames = ["MapScaleViewController"]
         
         //initialize map with a basemap
-        let map = AGSMap(basemap: AGSBasemap.topographic())
+        let map = AGSMap(basemap: AGSBasemap.streets())
         
         //assign the map to the map view
         self.mapView.map = map
@@ -39,15 +41,19 @@ class MapScaleViewController: UIViewController {
         pinch = UIPinchGestureRecognizer(target: self, action: #selector(self.mapPinchedByTouch(_:)))
         self.mapView.addGestureRecognizer(pinch)
         
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //set first extent to the map view
+        self.mapView.setViewpointCenter(AGSPoint(x: -117.1618385, y: 32.7065281 , spatialReference: AGSSpatialReference.wgs84()), scale: 25000, completion: nil)
+        
     }
     
     //User pinched on the map
     func mapPinchedByTouch(_ sender: UIPinchGestureRecognizer){
+        
+        self.showMapScale()
+    }
+    
+    // Shows the map scale
+    func showMapScale(){
         
         // scaleView.alpha default value set 0 in storyboard or viewDidLoad
         scaleView.alpha = 1
@@ -65,4 +71,85 @@ class MapScaleViewController: UIViewController {
         UIView.commitAnimations()
         
     }
+    
+    
+    //MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "MapScaleListSegue" {
+            let controller = segue.destination as! MapScaleListViewController
+            controller.delegate = self
+            controller.scales = self.scales
+            controller.presentationController?.delegate = self
+            controller.preferredContentSize = CGSize(width: 200, height: 200)
+        }
+    }
+    
+    //MARK: - OperationsListVCDelegate
+    
+    func mapScaleListViewController(_ mapScaleListViewController: MapScaleListViewController, didSelectOperation index: Int) {
+        
+        switch index {
+            
+        case 0: // 1/1000
+            self.mapView.setViewpointCenter(self.mapView.screen(toLocation: mapView.center), scale: 1000 , completion: {(success) -> Void in
+                if success {
+                    self.showMapScale();
+                }
+            })
+        case 1: // 1/5000
+            self.mapView.setViewpointCenter(self.mapView.screen(toLocation: mapView.center), scale: 5000 , completion: {(success) -> Void in
+                if success {
+                    self.showMapScale();
+                }
+            })
+            
+        case 2: // 1/25000
+            self.mapView.setViewpointCenter(self.mapView.screen(toLocation: mapView.center), scale: 25000 , completion: {(success) -> Void in
+                if success {
+                    self.showMapScale();
+                }
+            })
+            
+        case 3: // 1/50000
+            self.mapView.setViewpointCenter(self.mapView.screen(toLocation: mapView.center), scale: 50000 , completion: {(success) -> Void in
+                if success {
+                    self.showMapScale();
+                }
+            })
+            
+        case 4: // 1/100000
+            self.mapView.setViewpointCenter(self.mapView.screen(toLocation: mapView.center), scale: 100000 , completion: {(success) -> Void in
+                if success {
+                    self.showMapScale();
+                }
+            })
+            
+        case 5: // 1/1000000
+            self.mapView.setViewpointCenter(self.mapView.screen(toLocation: mapView.center), scale: 1000000 , completion: {(success) -> Void in
+                if success {
+                    self.showMapScale();
+                }
+            })
+            
+        case 6: // 1/5000000
+            self.mapView.setViewpointCenter(self.mapView.screen(toLocation: mapView.center), scale: 5000000 , completion: {(success) -> Void in
+                if success {
+                    self.showMapScale();
+                }
+            })
+            
+        default: break
+        }
+        
+    }
+    
+    
+    //MARK: - UIAdaptivePresentationControllerDelegate
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+        
+        return UIModalPresentationStyle.none
+    }
+    
 }

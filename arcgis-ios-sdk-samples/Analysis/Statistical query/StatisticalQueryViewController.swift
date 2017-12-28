962137ed8a2c2508c036bea15edf9c34315547e7
Nimesh Jarecha <njarecha@esri.com>
2017-12-28 15:25:02 -0800

    fixed method used for showing the error in two samples

--- arcgis-ios-sdk-samples/Analysis/Statistical query/StatisticalQueryViewController.swift
+++ arcgis-ios-sdk-samples/Analysis/Statistical query/StatisticalQueryViewController.swift
@@ -84,7 +84,7 @@ class StatisticalQueryViewController: UIViewController {
             //
             // If there an error, display it
             guard error == nil else {
-                SVProgressHUD.show(withStatus: error!.localizedDescription, maskType: .gradient)
+                SVProgressHUD.showError(withStatus: error!.localizedDescription, maskType: .gradient)
                 return
             }
             

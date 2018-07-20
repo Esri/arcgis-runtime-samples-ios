ed53a0fb2419f8337cf90d8ef797c6a6d01bac39
Philip Ridgeway <pridgeway@esri.com>
2018-07-20 13:59:45 -0700

    Uses a multiline string literal for source code html.

--- arcgis-ios-sdk-samples/Content Display Logic/Controllers/SourceCodeViewController.swift
+++ arcgis-ios-sdk-samples/Content Display Logic/Controllers/SourceCodeViewController.swift
@@ -59,15 +59,19 @@ class SourceCodeViewController: UIViewController, UIWebViewDelegate, UIAdaptiveP
         let cssPath = Bundle.main.path(forResource: "xcode", ofType: "css") ?? ""
         let jsPath = Bundle.main.path(forResource: "highlight.pack", ofType: "js") ?? ""
         let scale  = UIDevice.current.userInterfaceIdiom == .phone ? "0.5" : "1.0"
-        let stringForHTML = "<html> <head>" +
-            "<meta name='viewport' content='width=device-width, initial-scale='\(scale)'/> " +
-            "<link rel=\"stylesheet\" href=\"\(cssPath)\">" +
-            "<script src=\"\(jsPath)\"></script>" +
-            "<script>hljs.initHighlightingOnLoad();</script> </head> <body>" +
-            "<pre><code class=\"Swift\">\(content)</code></pre>" +
-            "</body> </html>"
-//        println(stringForHTML)
-        // style=\"white-space:initial;\"
+        let stringForHTML = """
+            <html>
+            <head>
+                <meta name='viewport' content='width=device-width, initial-scale='\(scale)'/>
+                <link rel="stylesheet" href="\(cssPath)">
+                <script src="\(jsPath)"></script>
+                <script>hljs.initHighlightingOnLoad();</script>
+            </head>
+            <body>
+                <pre><code class="Swift">\(content)</code></pre>
+            </body>
+            </html>
+            """
         return stringForHTML
     }
     

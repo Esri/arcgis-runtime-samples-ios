ccaeb4e0a8d869a901d6dc8b488d44e2a8f8134b
Philip Ridgeway <pridgeway@esri.com>
2018-09-07 11:36:43 -0700

    Refactors decoding categories in app delegate

--- arcgis-ios-sdk-samples/AppDelegate.swift
+++ arcgis-ios-sdk-samples/AppDelegate.swift
@@ -19,6 +19,31 @@ import ArcGIS
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
 
     var window: UIWindow?
+    
+    var splitViewController: UISplitViewController {
+        return window!.rootViewController as! UISplitViewController
+    }
+    var categoryBrowserViewController: ContentCollectionViewController {
+        return (splitViewController.viewControllers.first as! UINavigationController).viewControllers.first as! ContentCollectionViewController
+    }
+    
+    /// The URL of the content plist file inside the bundle.
+    var contentPlistURL: URL {
+        return Bundle.main.url(forResource: "ContentPList", withExtension: "plist")!
+    }
+    
+    /// Decodes an array of categories from the plist at the given URL.
+    ///
+    /// - Parameter url: The url of a plist that defines categories.
+    /// - Returns: An array of categories.
+    func decodeCategories(at url: URL) -> [Category] {
+        do {
+            let data = try Data(contentsOf: url)
+            return try PropertyListDecoder().decode([Category].self, from: data)
+        } catch {
+            fatalError("Error decoding categories at \(url): \(error)")
+        }
+    }
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
         if url.absoluteString.range(of: "auth", options: [], range: nil, locale: nil) != nil {
@@ -42,14 +67,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         splitViewController.maximumPrimaryColumnWidth = 320
         
         // Decode and populate Categories.
-        let contentCollectionViewController = (splitViewController.viewControllers.first as! UINavigationController).viewControllers.first as! ContentCollectionViewController
-        do {
-            let url = Bundle.main.url(forResource: "ContentPList", withExtension: "plist")!
-            let data = try Data(contentsOf: url)
-            contentCollectionViewController.categories = try PropertyListDecoder().decode([Category].self, from: data)
-        } catch {
-            fatalError("Error decoding categories: \(error)")
-        }
+        categoryBrowserViewController.categories = decodeCategories(at: contentPlistURL)
         
         self.modifyAppearance()
         

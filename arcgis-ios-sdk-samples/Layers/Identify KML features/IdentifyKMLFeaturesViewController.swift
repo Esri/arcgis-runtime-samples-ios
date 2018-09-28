ba33287daf88626222f5dcb2862cc5c3cb9a8fea
Philip Ridgeway <pridgeway@esri.com>
2018-09-28 14:42:32 -0700

    Identify KML Features sample
    Adds the Identify KML Features sample.

---
+++ arcgis-ios-sdk-samples/Layers/Identify KML features/IdentifyKMLFeaturesViewController.swift
@@ -0,0 +1,70 @@
+//
+// Copyright © 2018 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import UIKit
+import ArcGIS
+import WebKit
+
+/// A view controller that manages the interface of the Identify KML Features
+/// sample.
+class IdentifyKMLFeaturesViewController: UIViewController {
+    /// A KML layer with forecast data.
+    let forecastLayer: AGSKMLLayer = {
+        let url = URL(string: "https://www.wpc.ncep.noaa.gov/kml/noaa_chart/WPC_Day1_SigWx.kml")!
+        let dataSet = AGSKMLDataset(url: url)
+        return AGSKMLLayer(kmlDataset: dataSet)
+    }()
+    
+    /// The map view managed by the view controller.
+    @IBOutlet weak var mapView: AGSMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let map = AGSMap(basemap: .darkGrayCanvasVector())
+        map.operationalLayers.add(forecastLayer)
+        
+        let center = AGSPoint(x: -48_885, y: 1_718_235, spatialReference: AGSSpatialReference(wkid: 5070))
+        map.initialViewpoint = AGSViewpoint(center: center, scale: 50_000_000)
+        
+        mapView.map = map
+        
+        // Add the source code button item to the right of navigation bar.
+        (self.navigationItem.rightBarButtonItem as? SourceCodeBarButtonItem)?.filenames = ["IdentifyKMLFeaturesViewController"]
+    }
+    
+    func showCallout(for placemark: AGSKMLPlacemark, at point: AGSPoint) {
+        let webView = WKWebView(frame: CGRect(origin: .zero, size: CGSize(width: 320, height: 100)))
+        webView.backgroundColor = placemark.balloonBackgroundColor
+        webView.loadHTMLString(placemark.balloonContent, baseURL: nil)
+        mapView.callout.customView = webView
+        mapView.callout.show(at: point, screenOffset: .zero, rotateOffsetWithMap: false, animated: true)
+    }
+}
+
+extension IdentifyKMLFeaturesViewController: AGSGeoViewTouchDelegate {
+    func geoView(_ geoView: AGSGeoView, didTapAtScreenPoint screenPoint: CGPoint, mapPoint: AGSPoint) {
+        geoView.callout.dismiss()
+        geoView.identifyLayer(forecastLayer, screenPoint: screenPoint, tolerance: 15, returnPopupsOnly: false) { [weak self] (result) in
+            if let error = result.error {
+                print("Error identifying layer: \(error)")
+            } else if let placemarkIndex = result.geoElements.firstIndex(where: { $0 is AGSKMLPlacemark }) {
+                let placemark = result.geoElements[placemarkIndex] as! AGSKMLPlacemark
+                self?.showCallout(for: placemark, at: mapPoint)
+            }
+        }
+    }
+}

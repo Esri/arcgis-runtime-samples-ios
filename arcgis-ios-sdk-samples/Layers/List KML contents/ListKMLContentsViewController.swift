0bdea3f480516b9de178b0c98158932791d265e7
Quincy Morgan <qmorgan@esri.com>
2018-10-05 12:26:49 -0700

    Restructures the interface of the List KML Contents sample to use less memory and be more user-friendly

--- arcgis-ios-sdk-samples/Layers/List KML contents/ListKMLContentsViewController.swift
+++ arcgis-ios-sdk-samples/Layers/List KML contents/ListKMLContentsViewController.swift
@@ -17,14 +17,11 @@ import ArcGIS
 
 class ListKMLContentsViewController: UITableViewController {
     
-    var kmlDataset: AGSKMLDataset?
-    var parentNode: AGSKMLNode? {
-        didSet{
-            // set the navigation bar label to the node's name
-            title = parentNode?.name
-        }
-    }
-    var nodes: [AGSKMLNode] = []
+    private var kmlDataset: AGSKMLDataset?
+    
+    /// The full node tree flattened to an array of tuples that include each node's level.
+    /// Used as the model for the table view.
+    private var flattenedNodes: [(node: AGSKMLNode, level: Int)] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +31,7 @@ class ListKMLContentsViewController: UITableViewController {
         }
         
         // Add the source code button item to the right of navigation bar.
-        (navigationItem.rightBarButtonItem as! SourceCodeBarButtonItem).filenames = ["ListKMLContentsViewController"]
+        (navigationItem.rightBarButtonItem as! SourceCodeBarButtonItem).filenames = ["ListKMLContentsViewController", "ListKMLContentsSceneViewController"]
         
         // create the dataset from the local kml/kmz file with the given name
         let kmlDataset = AGSKMLDataset(name: "esri_test_data")
@@ -55,25 +52,22 @@ class ListKMLContentsViewController: UITableViewController {
                 return
             }
             
-            /// Sets `isVisible` to `true` for these nodes and their descendants
-            func makeNodesVisible(_ nodes: [AGSKMLNode]){
+            self.flattenedNodes = []
+            func addNodes(_ nodes: [AGSKMLNode], level: Int){
                 for node in nodes{
                     node.isVisible = true
-                    makeNodesVisible(self.childNodes(of: node))
+                    self.flattenedNodes.append((node, level))
+                    addNodes(self.childNodes(of: node), level: level+1)
                 }
             }
+            addNodes(kmlDataset.rootNodes, level: 0)
             
-            // some nodes are not visible by default so ensure that all of them are
-            makeNodesVisible(kmlDataset.rootNodes)
-            
-            self.nodes = kmlDataset.rootNodes
-            // populate the outline view now that the dataset is loaded
+            // populate the outline view now that the data is loaded
             self.tableView.reloadData()
-
         }
     }
     
-    /// Returns all the child nodes of this node
+    /// Returns all the child nodes of this node.
     private func childNodes(of node: AGSKMLNode) -> [AGSKMLNode] {
         if let container = node as? AGSKMLContainer {
             return container.childNodes
@@ -86,45 +80,20 @@ class ListKMLContentsViewController: UITableViewController {
     
     //MARK: - UITableViewDataSource
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        if nodes.isEmpty {
-            return 1
-        }
-        return 2
-    }
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 1 || nodes.isEmpty {
-            return 1
-        }
-        return nodes.count
+        return flattenedNodes.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 1 || nodes.isEmpty {
-            // get and setup a cell to display the scene
-            let cell = tableView.dequeueReusableCell(withIdentifier: "ListKMLContentsSceneCell") as! ListKMLContentsSceneCell
-            cell.kmlDataset = kmlDataset
-            cell.node = parentNode
-            return cell
-        }
         // get and setup a cell to display the node info
         let cell = tableView.dequeueReusableCell(withIdentifier: "NodeCell")!
-        let node = nodes[indexPath.row]
+        let flattenedNodeInfo = flattenedNodes[indexPath.row]
+        let node = flattenedNodeInfo.node
         cell.textLabel?.text = node.name
         cell.detailTextLabel?.text = "\(type(of:node))"
+        // set the indentation based on the node hierarchy
+        cell.indentationLevel = flattenedNodeInfo.level
         return cell
-        
-    }
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if section == 1 || nodes.isEmpty {
-            return "Scene"
-        }
-        if parentNode != nil {
-            return "Child Nodes"
-        }
-        return "Root Nodes"
     }
     
     //MARK: - UITableViewDelegate
@@ -133,175 +102,12 @@ class ListKMLContentsViewController: UITableViewController {
         guard indexPath.section == 0 else{
             return
         }
-        let node = nodes[indexPath.row]
-        // create and show a child table view controller for this node
-        let tableViewController = storyboard?.instantiateViewController(withIdentifier: "ListKMLContentsViewController") as! ListKMLContentsViewController
-        tableViewController.kmlDataset = kmlDataset
-        tableViewController.parentNode = node
-        tableViewController.nodes = childNodes(of: node)
-        show(tableViewController, sender: self)
+        let node = flattenedNodes[indexPath.row].node
+        // create and show a view controller showing the scene and the extent of this node
+        let viewController = storyboard?.instantiateViewController(withIdentifier: "ListKMLContentsSceneViewController") as! ListKMLContentsSceneViewController
+        viewController.kmlDataset = kmlDataset
+        viewController.node = node
+        show(viewController, sender: self)
     }
     
-    override func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
-        return indexPath.section == 0 && !nodes.isEmpty
-    }
-    
-}
-
-class ListKMLContentsSceneCell: UITableViewCell {
-    
-    @IBOutlet weak var sceneView: AGSSceneView!
-    
-    weak var kmlDataset: AGSKMLDataset? {
-        didSet{
-            
-            guard let kmlDataset = kmlDataset else {
-                return
-            }
-            
-            // initialize scene with labeled imagery
-            let scene = AGSScene(basemapType: .imageryWithLabels)
-            
-            // create an elevation source and add it to the scene's surface
-            let elevationSourceURL = URL(string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!
-            let elevationSource = AGSArcGISTiledElevationSource(url: elevationSourceURL)
-            scene.baseSurface?.elevationSources.append(elevationSource)
-            
-            // assign the scene to the view
-            sceneView.scene = scene
-            
-            // create a layer to display the dataset
-            let kmlLayer = AGSKMLLayer(kmlDataset: kmlDataset)
-            
-            // add the kml layer to the map
-            scene.operationalLayers.add(kmlLayer)
-        }
-    }
-    
-    weak var node: AGSKMLNode? {
-        didSet{
-            setSceneViewpoint()
-        }
-    }
-    
-    //MARK: - Viewpoint
-    
-    private func setSceneViewpoint(){
-        if let node = node{
-            if let nodeViewpoint = viewpoint(for: node),
-                !nodeViewpoint.targetGeometry.isEmpty {
-                // set the viewpoint for the node
-                sceneView.setViewpoint(nodeViewpoint)
-            }
-            else {
-                // this node has no viewpoint so hide the scene view, showing the info text
-                sceneView.isHidden = true
-            }
-        }
-        else {
-            // this cell is in the root view controller of the sample, so just show the whole layer
-            if let operationalLayers = sceneView.scene?.operationalLayers as? [Any],
-                let kmlLayer = operationalLayers.last as? AGSKMLLayer,
-                let extent = kmlLayer.fullExtent {
-                // set the viewpoint based on the layer's geometry extent
-                sceneView.setViewpoint(AGSViewpoint(targetExtent: extent))
-            }
-        }
-    }
-    
-    /// Returns the elevation of the scene's surface corresponding to the point's x/y.
-    private func sceneSurfaceElevation(for point: AGSPoint) -> Double? {
-        guard let surface = sceneView.scene?.baseSurface else {
-            return nil
-        }
-        
-        var surfaceElevation: Double? = nil
-        let group = DispatchGroup()
-        group.enter()
-        // we want to return the elevation synchronously, so run the task in the background and wait
-        DispatchQueue.global(qos: .userInteractive).async {
-            surface.elevation(for: point, completion: { (elevation, error) in
-                if error == nil{
-                    surfaceElevation = elevation
-                }
-                group.leave()
-            })
-        }
-        // wait, but not longer than three seconds
-        _ = group.wait(timeout: .now() + 3)
-        return surfaceElevation
-    }
-    
-    /// Returns the viewpoint showing the node, converting it from the node's AGSKMLViewPoint if possible.
-    private func viewpoint(for node: AGSKMLNode) -> AGSViewpoint? {
-        
-        if let kmlViewpoint = node.viewpoint {
-            // Convert the KML viewpoint to a viewpoint for the scene.
-            // The KML viewpoint may not correspond to the node's geometry.
-            
-            switch kmlViewpoint.type{
-            case .lookAt:
-                var lookAtPoint = kmlViewpoint.location
-                if kmlViewpoint.altitudeMode != .absolute{
-                    // if the elevation is relative, account for the surface's elevation
-                    let elevation = sceneSurfaceElevation(for: lookAtPoint) ?? 0
-                    lookAtPoint = AGSPoint(x: lookAtPoint.x, y: lookAtPoint.y, z: lookAtPoint.z + elevation, spatialReference: lookAtPoint.spatialReference)
-                }
-                let camera = AGSCamera(lookAt: lookAtPoint,
-                                       distance: kmlViewpoint.range,
-                                       heading: kmlViewpoint.heading,
-                                       pitch: kmlViewpoint.pitch,
-                                       roll: kmlViewpoint.roll)
-                // only the camera parameter is used by the scene
-                return AGSViewpoint(center: kmlViewpoint.location, scale: 1, camera: camera)
-            case .camera:
-                // convert the KML viewpoint to a camera
-                let camera = AGSCamera(location: kmlViewpoint.location,
-                                       heading: kmlViewpoint.heading,
-                                       pitch: kmlViewpoint.pitch,
-                                       roll: kmlViewpoint.roll)
-                // only the camera parameter is used by the scene
-                return AGSViewpoint(center: kmlViewpoint.location, scale: 1, camera: camera)
-            case .unknown:
-                print("Unexpected AGSKMLViewpointType \(kmlViewpoint.type)")
-                return nil
-            }
-        }
-            // the node does not have a predefined viewpoint, so create a viewpoint based on its extent
-        else if let extent = node.extent,
-            // some nodes do not include a geometry, so check that the extent isn't empty
-            !extent.isEmpty {
-            
-            var center = extent.center
-            // take the scene's elevation into account
-            let elevation = sceneSurfaceElevation(for: center) ?? 0
-            
-            // It's possible for `isEmpty` to be false but for width/height to still be zero.
-            if extent.width == 0,
-                extent.height == 0 {
-                
-                center = AGSPoint(x: center.x, y: center.y, z: center.z + elevation, spatialReference: extent.spatialReference)
-                // Defaults based on Google Earth.
-                let camera = AGSCamera(lookAt: center, distance: 1000, heading: 0, pitch: 45, roll: 0)
-                // only the camera parameter is used by the scene
-                return AGSViewpoint(targetExtent: extent, camera: camera)
-                
-            }
-            else {
-                // expand the extent to give some margins when framing the node
-                let bufferRadius = [extent.width, extent.height].max()! / 20
-                let bufferedExtent = AGSEnvelope(xMin: extent.xMin - bufferRadius,
-                                                 yMin: extent.yMin - bufferRadius,
-                                                 zMin: extent.zMin - bufferRadius + elevation,
-                                                 xMax: extent.xMax + bufferRadius,
-                                                 yMax: extent.yMax + bufferRadius,
-                                                 zMax: extent.zMax + bufferRadius + elevation,
-                                                 spatialReference: .wgs84())
-                return AGSViewpoint(targetExtent: bufferedExtent)
-            }
-        }
-        // the node doesn't have a predefined viewpoint or geometry
-        return nil
-    }
-
 }
--- arcgis-ios-sdk-samples/Layers/List KML contents/README.md
+++ arcgis-ios-sdk-samples/Layers/List KML contents/README.md
@@ -6,14 +6,13 @@ List the contents of a KML file. KML files can contain a hierarchy of features, 
 
 ## How to use the sample
 
-The root nodes of the KML file are shown in the table view. Tap a node to see its children and its extent, shown in a scene view. Not all nodes have an extent (e.g. screen overlays).
+All nodes of the KML file are shown in the table view, with the hierarchy indicated by indentation. Tap a node to see its extent in a scene view. Not all nodes have an extent (e.g. screen overlays).
 
 ## How it works
 
 1. A local KML file is loaded as an `AGSKMLDataset` to be used throughout the sample.
-2. All KML nodes are recursively set to be visible since some nodes may not be visible by default.
-3. The nodes of the `AGSKMLDataset` are explored recursively to populate the nested table views, starting with the `rootNodes` property.
-4. In each table where the node has an extent, the dataset is shown as a `AGSKMLLayer` in a `AGSSceneView`. A viewpoint for the node is created, if possible, and set with the `setViewpoint` function of `AGSSceneView`.
+2. All KML nodes are recursively set to be visible—since some may not be visible by default—and loaded into a array to use as the table's data source, starting with the `rootNodes` property.
+3. When a cell is tapped, the entire KML dataset is shown as a `AGSKMLLayer` in a `AGSSceneView`. A viewpoint for the node is created, if possible, and set with the `setViewpoint` function of `AGSSceneView`.
 
 ## Relevant API
 

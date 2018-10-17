7e1f3e85ba4167dc5590945289231f16c838e100
Philip Ridgeway <pridgeway@esri.com>
2018-10-17 16:15:22 -0700

    Raster Function (Service): Use string literal for raster function

--- arcgis-ios-sdk-samples/Layers/Raster function (service)/RasterFunctionServiceViewController.swift
+++ arcgis-ios-sdk-samples/Layers/Raster function (service)/RasterFunctionServiceViewController.swift
@@ -53,39 +53,23 @@ class RasterFunctionServiceViewController: UIViewController {
     }
     
     func applyRasterFunction() {
-        //
-        // NOTE: This is the ASCII text for actual raw JSON string:
-        // ========================================================
-        //{
-        //  "raster_function_arguments":
-        //  {
-        //    "z_factor":{"double":25.0,"type":"Raster_function_variable"},
-        //    "slope_type":{"raster_slope_type":"none","type":"Raster_function_variable"},
-        //    "azimuth":{"double":315,"type":"Raster_function_variable"},
-        //    "altitude":{"double":45,"type":"Raster_function_variable"},
-        //    "type":"Raster_function_arguments",
-        //    "raster":{"name":"raster","is_raster":true,"type":"Raster_function_variable"},
-        //    "nbits":{"int":8,"type":"Raster_function_variable"}
-        //  },
-        //  "raster_function":{"type":"Hillshade_function"},
-        //  "type":"Raster_function_template"
-        //}
-        
         // Define the JSON string needed for the raster function
-        let rasterFunctionJSONString = "{" +
-            "\"raster_function_arguments\":" +
-            "{" +
-            "    \"z_factor\":{\"double\":25.0,\"type\":\"Raster_function_variable\"}," +
-            "    \"slope_type\":{\"raster_slope_type\":\"none\",\"type\":\"Raster_function_variable\"}," +
-            "    \"azimuth\":{\"double\":315,\"type\":\"Raster_function_variable\"}," +
-            "    \"altitude\":{\"double\":45,\"type\":\"Raster_function_variable\"}," +
-            "    \"type\":\"Raster_function_arguments\"," +
-            "    \"raster\":{\"name\":\"raster\",\"is_raster\":true,\"type\":\"Raster_function_variable\"}," +
-            "    \"nbits\":{\"int\":8,\"type\":\"Raster_function_variable\"}" +
-            "}," +
-            "\"raster_function\":{\"type\":\"Hillshade_function\"}," +
-            "\"type\":\"Raster_function_template\"" +
-        "}"
+        let rasterFunctionJSONString = """
+            {
+              "raster_function_arguments":
+              {
+                "z_factor":{"double":25.0,"type":"Raster_function_variable"},
+                "slope_type":{"raster_slope_type":"none","type":"Raster_function_variable"},
+                "azimuth":{"double":315,"type":"Raster_function_variable"},
+                "altitude":{"double":45,"type":"Raster_function_variable"},
+                "type":"Raster_function_arguments",
+                "raster":{"name":"raster","is_raster":true,"type":"Raster_function_variable"},
+                "nbits":{"int":8,"type":"Raster_function_variable"}
+              },
+              "raster_function":{"type":"Hillshade_function"},
+              "type":"Raster_function_template"
+            }
+            """
 
         // NOTE: You can alternatively create the raster function via a JSON string that is contained in a
         // file on disk (ex: hillshade_simplified.json) via the constructor: AGSRasterFunction(fileURL: <#T##URL#>)

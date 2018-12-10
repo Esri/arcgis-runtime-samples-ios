28323200052a7488fa4772d7bb51c215863ee25d
Quincy Morgan <qmorgan@esri.com>
2018-12-10 15:04:08 -0800

    Converts the Manage sublayers settings to a popover

--- arcgis-ios-sdk-samples/Layers/Manage sublayers/ManageSublayersViewController.swift
+++ arcgis-ios-sdk-samples/Layers/Manage sublayers/ManageSublayersViewController.swift
@@ -105,10 +105,21 @@ class ManageSublayersViewController: UIViewController, MapImageSublayersVCDelega
             let navigationController = segue.destination as? UINavigationController,
             let controller = navigationController.viewControllers.first as? MapImageSublayersVC {
             
+            navigationController.presentationController?.delegate = self
+            
             controller.delegate = self
             controller.preferredContentSize = CGSize(width: 300, height: 300)
             controller.mapImageLayer = self.mapImageLayer
             controller.removedMapImageSublayers = self.removedMapImageSublayers
         }
     }
+}
+
+extension ManageSublayersViewController: UIAdaptivePresentationControllerDelegate {
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+        // present as popover even in size-constrained environments
+        return .none
+    }
+    
 }

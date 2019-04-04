28c36bee83b6c6c27d4307085b44903155d7ee81
gari7838 <gdhakal@esri.com>
2019-04-04 15:59:30 -0700

    Add group layers sample

---
+++ arcgis-ios-sdk-samples/Layers/Group layers/GroupLayersViewController.swift
@@ -0,0 +1,148 @@
+// Copyright 2019 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import UIKit
+import ArcGIS
+
+class GroupLayersViewController: UIViewController {
+    
+    @IBOutlet var sceneView: AGSSceneView!
+    @IBOutlet var layersBBI: UIBarButtonItem!
+    
+    let scene: AGSScene
+    let groupLayerName = "Dev A"
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+        // Initialize a scene with imagery basemap.
+        scene = AGSScene(basemap: .imagery())
+        
+        // Add base surface to the scene for elevation data.
+        let surface = AGSSurface()
+        let worldElevationServiceURL = URL(string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!
+        let elevationSource = AGSArcGISTiledElevationSource(url: worldElevationServiceURL)
+        surface.elevationSources.append(elevationSource)
+        scene.baseSurface = surface
+        
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        (self.navigationItem.rightBarButtonItem as! SourceCodeBarButtonItem).filenames = ["GroupLayersViewController", "LayersTableViewController"]
+        
+        self.layersBBI.isEnabled = false
+        
+        // Assign the scene to the sceneView.
+        sceneView.scene = scene
+    
+        addOperationalLayers()
+        zoomToGroupLayer()
+    }
+    
+    /// Zooms to the extent of the Group Layer when its child layers are loaded.
+    func zoomToGroupLayer() {
+        
+        let dispatchGroup = DispatchGroup()
+        
+        guard let groupLayer = (scene.operationalLayers as? [AGSLayer])?.filter({$0.name == groupLayerName}).first as? AGSGroupLayer else { return }
+        
+        for layer in groupLayer.layers as? [AGSLayer] ?? [] {
+            dispatchGroup.enter()
+            layer.load(completion: { (error) in
+                dispatchGroup.leave()
+            })
+        }
+        
+        dispatchGroup.notify(queue: .main) { [weak self] in
+            guard let groupLayer = (self?.scene.operationalLayers as? [AGSLayer])?.filter({$0.name == self?.groupLayerName}).first as? AGSGroupLayer else { return }
+            
+            if let extent = groupLayer.fullExtent {
+                let camera = AGSCamera(lookAt: extent.center, distance: 700, heading: 0, pitch: 60, roll: 0)
+                
+                // Zoom to the viewpoint specified by the camera position.
+                self?.sceneView.setViewpointCamera(camera, completion: { (_) in
+                    DispatchQueue.main.async {
+                        // Enable the bar button item to display
+                        // the Table of Contents of operational layers.
+                        self?.layersBBI.isEnabled = true
+                    }
+                })
+            }
+        }
+    }
+    
+    /// Adds a group layer and other layers to the scene as operational layers.
+    func addOperationalLayers() {
+        
+        let sceneLayer = AGSArcGISSceneLayer(url: URL(string: "https://scenesampleserverdev.arcgis.com/arcgis/rest/services/Hosted/PlannedDemo_BuildingShell/SceneServer/layers/0")!)
+        
+        let featureTable = AGSServiceFeatureTable(url: URL(string: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/DevelopmentProjectArea/FeatureServer/0")!)
+        let featureLayer = AGSFeatureLayer(featureTable: featureTable)
+        
+        scene.operationalLayers.addObjects(from: [makeGroupLayer(), sceneLayer, featureLayer])
+    }
+    
+    /// Returns a group layer.
+    func makeGroupLayer() -> AGSGroupLayer {
+
+        // Create a group layer and set its name.
+        let groupLayer = AGSGroupLayer()
+        groupLayer.name = groupLayerName
+        
+        // Create two scene layers.
+        let trees = AGSArcGISSceneLayer(url: URL(string: "https://scenesampleserverdev.arcgis.com/arcgis/rest/services/Hosted/DevA_Trees/SceneServer/layers/0")!)
+        let pathways = AGSArcGISSceneLayer(url: URL(string: "https://scenesampleserverdev.arcgis.com/arcgis/rest/services/Hosted/DevA_Pathways/SceneServer/layers/0")!)
+        
+        // Create a feature layer.
+        let buildings = AGSArcGISSceneLayer(url: URL(string: "https://scenesampleserverdev.arcgis.com/arcgis/rest/services/Hosted/DevA_BuildingShell_Textured/SceneServer/layers/0")!)
+        
+        // Add the scene layers and feature layer as children of the group layer.
+        groupLayer.layers.add(trees)
+        groupLayer.layers.add(pathways)
+        groupLayer.layers.add(buildings)
+        
+        return groupLayer
+    }
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "LayersPopover" {
+            guard let controller = segue.destination as? LayersTableViewController else { return }
+            
+            if let layers = scene.operationalLayers as? [AGSLayer] {
+                controller.layers.removeAll()
+                controller.layers.append(contentsOf: layers)
+            }
+            
+            // Popover presentation logic.
+            controller.presentationController?.delegate = self
+            controller.preferredContentSize = CGSize(width: 300, height: 200)
+        }
+    }
+}
+
+
+extension GroupLayersViewController: UIAdaptivePresentationControllerDelegate {
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+        
+        // For popover or non modal presentation.
+        return .none
+    }
+}
+
---
+++ arcgis-ios-sdk-samples/Layers/Group layers/LayersTableViewController.swift
@@ -0,0 +1,216 @@
+// Copyright 2019 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import UIKit
+import ArcGIS
+
+class LayersTableViewController: UITableViewController {
+    
+    var layers = [AGSLayer]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTableView()
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return layers.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return layers.getItem(at: section)?.name
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        guard let groupLayer = layers.getItem(at: section) as? AGSGroupLayer else { return 0 }
+        return groupLayer.layers.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "layerCell", for: indexPath)
+        configure(cell)
+        
+        // Set label.
+        guard let groupLayer = layers.getItem(at: indexPath.section) as? AGSGroupLayer, let childLayers = groupLayer.layers as? [AGSLayer], let childLayer = childLayers.getItem(at: indexPath.row) else { return cell }
+        cell.textLabel?.text = formattedValue(of: childLayer.name)
+        
+        // Set accessory view.
+        cell.accessoryView = makeAccessoryView(section: indexPath.section, row: indexPath.row)
+        
+        return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "layerCell")
+        configure(cell, asHeaderView: true)
+        
+        // Set label.
+        guard let layer = layers.getItem(at: section) else { return cell }
+        cell.textLabel?.text = formattedValue(of: layer.name)
+        
+        // Set accessory view.
+        cell.accessoryView = makeAccessoryView(section: section)
+       
+        return cell
+    }
+    
+    // MARK: - Action
+    
+    @objc func switchChanged(_ sender: CustomSwitch) {
+        
+        guard let section = sender.section, let layer = layers.getItem(at: section) else { return }
+        let isSwitchOn = sender.isOn
+        
+        if let row = sender.row, let groupLayer = layers.getItem(at: section) as? AGSGroupLayer, let childLayers = groupLayer.layers as? [AGSLayer], let childLayer = childLayers.getItem(at: row) {
+            
+            childLayer.isVisible = isSwitchOn
+        }
+        else if layer is AGSGroupLayer {
+            layer.isVisible = isSwitchOn
+            updateSwitchForRows(in: section, enabled: isSwitchOn)
+        }
+        else {
+            layer.isVisible = isSwitchOn
+        }
+    }
+    
+    // MARK: - Helper methods
+    
+    /// Makes an accessory view for a cell in table view.
+    ///
+    /// - Parameters:
+    ///     - section: Section number of the table view.
+    ///     - row: Row number of the table view section.
+    /// - Returns: A custom UISwitch.
+    func makeAccessoryView(section: Int, row: Int? = nil) -> CustomSwitch? {
+        
+        // Create a switch.
+        let visibilitySwitch = CustomSwitch(frame: .zero)
+        visibilitySwitch.section = section
+        visibilitySwitch.addTarget(self, action: #selector(LayersTableViewController.switchChanged(_:)), for: .valueChanged)
+        
+        // Set the switch to on or off.
+        guard let layer = layers.getItem(at: section) else { return nil }
+        visibilitySwitch.isOn = layer.isVisible
+        
+        if let row = row, let groupLayer = layer as? AGSGroupLayer, let childLayers = groupLayer.layers as? [AGSLayer], let childLayer = childLayers.getItem(at: row) {
+            
+            visibilitySwitch.row = row
+            visibilitySwitch.isOn = childLayer.isVisible
+        }
+        
+        // Disable switch.
+        if layer is AGSGroupLayer, !layer.isVisible {
+            updateSwitchForRows(in: section, enabled: false)
+        }
+        
+        return visibilitySwitch
+    }
+    
+    /// Ensures we cannot toggle visibility of child layers when the parent is turned off.
+    ///
+    /// - Parameters:
+    ///     - section: Section number of the table view.
+    ///     - enabled: Indicates if the switch should be enabled or disabled.
+    func updateSwitchForRows(in section: Int, enabled: Bool) {
+        
+        guard let groupLayer = layers.getItem(at: section) as? AGSGroupLayer else { return }
+        
+        for index in 0..<groupLayer.layers.count {
+            let cell = tableView.cellForRow(at: IndexPath(row: index, section: section))
+            
+            if let visibilitySwitch = cell?.accessoryView as? CustomSwitch {
+                visibilitySwitch.isEnabled = enabled
+            }
+        }
+    }
+    
+    /// Configures table view.
+    func configureTableView() {
+        
+        tableView.sectionHeaderHeight = UITableView.automaticDimension
+        tableView.estimatedSectionHeaderHeight = 25;
+        
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 25
+        
+        tableView.isEditing = false
+    }
+    
+    /// Configures table view cell.
+    ///
+    /// - Parameters:
+    ///     - cell: Table view cell to configure.
+    ///     - asHeaderView: Indicates if the cell should be configured for table view section.
+    func configure(_ cell: UITableViewCell, asHeaderView: Bool = false) {
+        
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.lineBreakMode = .byWordWrapping
+        
+        if asHeaderView {
+            cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        }
+        else {
+            cell.selectionStyle = .none
+            cell.separatorInset = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 0)
+        }
+    }
+    
+    /// Modifies name of the layer.
+    ///
+    /// - Parameter name: Original name of the layer.
+    /// - Returns: A modified name or the original name of the layer.
+    func formattedValue(of name: String) -> String {
+        
+        switch name {
+        case "DevA_Trees":
+            return "Dev A: Trees"
+        case "DevA_Pathways":
+            return "Dev A: Pathways"
+        case "DevA_BuildingShell_Textured":
+            return "Dev A: Building Shell Textured"
+        case "PlannedDemo_BuildingShell":
+            return "Planned Demo Building Shell"
+        case "DevelopmentProjectArea":
+            return "Development Project Area"
+        default:
+            return name
+        }
+    }
+}
+
+
+class CustomSwitch: UISwitch {
+    
+    var section: Int?
+    var row: Int?
+}
+
+
+extension Array {
+    
+    func getItem(at index: Int) -> Element? {
+        
+        guard index > -1, index < count else { return nil }
+        return self[index]
+    }
+}
+

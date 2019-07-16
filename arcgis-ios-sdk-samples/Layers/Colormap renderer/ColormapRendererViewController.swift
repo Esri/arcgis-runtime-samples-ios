531d1b66c64c8822f4e53e94380fba29b463da56
Vivian <vquach@esri.com>
2019-07-15 17:00:11 -0700

    Added imports

--- arcgis-ios-sdk-samples/Layers/Colormap renderer/ColormapRendererViewController.swift
+++ arcgis-ios-sdk-samples/Layers/Colormap renderer/ColormapRendererViewController.swift
@@ -1,5 +1,5 @@
 //
-// Copyright 2016 Esri.
+// Copyright 2019 Esri.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,5 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import Foundation
+import UIKit
 import ArcGIS
+
+

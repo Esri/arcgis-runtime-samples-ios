29108fc5464cc47b475d568145d3057937479969
Vivian <vquach@esri.com>
2019-07-30 17:40:08 -0700

    Displays features

--- arcgis-ios-sdk-samples/Edit data/Edit and sync features/EditAndSyncFeaturesViewController.swift
+++ arcgis-ios-sdk-samples/Edit data/Edit and sync features/EditAndSyncFeaturesViewController.swift
@@ -27,7 +27,6 @@ class EditAndSyncFeaturesViewController: UIViewController {
             
             // Assign the map to the map view.
             mapView.map = map
-            mapView.setViewpoint(AGSViewpoint(targetExtent: tileCache.fullExtent!))
 
             // Display graphics overlay of the download area.
 //            displayGraphics()
@@ -46,22 +45,79 @@ class EditAndSyncFeaturesViewController: UIViewController {
             //setup extent view
             extentView.layer.borderColor = UIColor.red.cgColor
             extentView.layer.borderWidth = 3
+            
+            // Create a geodatabase sync task using the feature service URL.
+            let featureServiceString = "https://sampleserver6.arcgisonline" + ".com/arcgis/rest/services/Sync/WildfireSync/FeatureServer"
+            let featureServiceURL = URL(string: featureServiceString)
+            geodatabaseSyncTask = AGSGeodatabaseSyncTask(url: featureServiceURL!)
+            geodatabaseSyncTask.load { [weak self] (error: Error?) in
+                if let error = error {
+                    self?.presentAlert(error: error)
+                } else {
+                    for layerInfo in self!.geodatabaseSyncTask.featureServiceInfo!.layerInfos {
+                        let featureLayerURL = URL(string: featureServiceString + "/" + String(layerInfo.id))
+                        let onlineFeatureTable = AGSServiceFeatureTable(url: featureLayerURL!)
+                        
+                        onlineFeatureTable.load { (error: Error?) in
+                            if let error = error {
+                                self?.presentAlert(error: error)
+                                //don't forget to add an error display message here////////////////////!!!!!!!!!!!!!!!!!!//////////////!!!!!!!!!!!!!
+                            } else {
+                                if onlineFeatureTable.geometryType == AGSGeometryType.point {
+                                    self?.mapView.map?.operationalLayers.add(AGSFeatureLayer(featureTable: onlineFeatureTable))
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
     @IBOutlet private var generateButton: UIButton!
     @IBOutlet private var syncButton: UIButton!
     @IBOutlet private var progressBar: UIProgressView!
     
-    private var downloadAreaGraphic: AGSGraphic! {
-        didSet {
-    
-        }
-    }
-    
+    private var downloadAreaGraphic: AGSGraphic!
     private var geodatabaseSyncTask: AGSGeodatabaseSyncTask!
     private var geodatabase: AGSGeodatabase!
     private var viewpointChangedListener: UITableViewDropCoordinator!
     private var selectedFeature: AGSFeature!
+    
+    func extentViewFrameToEnvelope() -> AGSEnvelope {
+        let frame = mapView.convert(extentView.frame, from: view)
+        
+        //the lower-left corner
+        let minPoint = mapView.screen(toLocation: frame.origin)
+        
+        //the upper-right corner
+        let maxPoint = mapView.screen(toLocation: CGPoint(x: frame.maxX, y: frame.maxY))
+        
+        //return the envenlope covering the entire extent frame
+        return AGSEnvelope(min: minPoint, max: maxPoint)
+    }
+    
+    func initialize() {
+       
+    }
+    
+    @IBAction func generateOfflineMapAction() {
+        // Hide the unnecessary items.
+        generateButton.isEnabled = false
+        extentView.isHidden = true
+        
+        // Get the area outlined by the extent view.
+        let areaOfInterest = extentViewFrameToEnvelope()
+        
+        /////////add a progress or loading view//////////////////!!!!!!!!!!!!!!!!!///////////////////!!!!!!!!!!!!!!!!//////////////////
+        let fileManager = FileManager.default
+//        let currentDirectory = Bundle.main.resourcePath
+        let temporaryFile = FileManager.createFile(fileManager)
+        // DON'T FORGET TO DELETE FILE ONCE DONE!!!!!!!!//////////////!!!!!!!!!!!!!!!!//////////////!!!!!!!!!!!!!!!!!
+//        let geodatabaseParameters = geodatabaseSyncTask.AGSGenerateGeodatabaseParameters()
+//        let generateGeodatabaseJob = geodatabaseSyncTask.generateJob(with: <#T##AGSGenerateGeodatabaseParameters#>, downloadFileURL: <#T##URL#>)
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()

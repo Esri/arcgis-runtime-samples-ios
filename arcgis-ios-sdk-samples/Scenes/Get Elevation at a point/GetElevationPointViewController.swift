ff2ae7aadeef100b6e0df027a3c6c0f3d2b67460
Vivian <vquach@esri.com>
2019-08-08 16:59:14 -0700

    Created new sample
    Need to edit size of scene view. Edited contentplist, added new group and files.

---
+++ arcgis-ios-sdk-samples/Scenes/Get Elevation at a point/GetElevationPointViewController.swift
@@ -0,0 +1,31 @@
+// Copyright 2019 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import UIKit
+import ArcGIS
+
+class GetElevationPointViewController: UIViewController {
+    @IBOutlet private weak var sceneView: AGSSceneView! {
+        didSet {
+            //initialize the map
+            mapView.map = AGSMap(basemap: .nationalGeographic())
+            mapView.setViewpointCenter(AGSPoint(x: -1.2e7, y: 5e6, spatialReference: .webMercator()), scale: 4e7)
+        }
+}
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //add the source code button item to the right of navigation bar
+        (self.navigationItem.rightBarButtonItem as! SourceCodeBarButtonItem).filenames = ["GetElevationPointViewController"]
+    }
+}

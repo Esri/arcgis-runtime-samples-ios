5960d36d99ffecaf7a36dcc62d72294edb6ee9ee
Steve Baranski <steven.baranski@icloud.com>
2019-08-09 09:24:57 -0700

    Setup storyboard and integrated with view controller

--- arcgis-ios-sdk-samples/Maps/Show location history/LocationHistoryViewController.swift
+++ arcgis-ios-sdk-samples/Maps/Show location history/LocationHistoryViewController.swift
@@ -17,4 +17,60 @@
 import UIKit
 import ArcGIS
 
-class LocationHistoryViewController: UIViewController {}
+// MARK: - Constants
+
+private enum Constants {
+    static let initialLatitude: Double = 34.056295
+    static let initialLongitude: Double = -117.195800
+    static let levelOfDetail: Int = 18
+}
+
+// MARK: - LocationHistoryViewController
+
+class LocationHistoryViewController: UIViewController {
+    @IBOutlet weak var mapView: AGSMapView!
+    @IBOutlet weak var trackingBarButtonItem: UIBarButtonItem!
+    
+    private let map = AGSMap(basemapType: .lightGrayCanvasVector, latitude: Constants.initialLatitude, longitude: Constants.initialLongitude, levelOfDetail: Constants.levelOfDetail)
+    
+    // MARK: UIViewController
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.isToolbarHidden = false
+    }
+    
+    // MARK: IBActions
+    
+    @IBAction private func trackingTapped(_ sender: UIBarButtonItem) {}
+    
+    // MARK: Private behavior
+    
+    private func setupMapView() {
+        mapView.map = map
+    }
+    
+    private func setupToolbar() {
+        let leadingSpaceToolbarItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let trailingSpaceToolbarItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        toolbarItems = [
+            leadingSpaceToolbarItem,
+            trackingBarButtonItem,
+            trailingSpaceToolbarItem
+        ]
+    }
+    
+    private func setupView() {
+        setupMapView()
+        
+        setupToolbar()
+    }
+}

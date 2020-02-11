02bf09a7f95abdfd939d65eb9f8fb7e97a85ac7c
Vivian Quach <viv10382@esri.com>
2020-02-11 14:07:55 -0800

    Added table popover and switch

--- arcgis-ios-sdk-samples/Features/Display subtype feature layer/DisplaySubtypeFeatureLayerViewController.swift
+++ arcgis-ios-sdk-samples/Features/Display subtype feature layer/DisplaySubtypeFeatureLayerViewController.swift
@@ -17,12 +17,64 @@ import ArcGIS
 
 class DisplaySubtypeFeatureLayerViewController: UIViewController {
     
-    @IBOutlet private weak var mapView: AGSMapView! {
+//    @IBAction private weak var sublayerSwitch: UISwitch!
+    
+    var subtypeFeatureLayer: AGSSubtypeFeatureLayer? {
         didSet {
-            //initialize the map
-            mapView.map = AGSMap(basemap: .streetsNightVector())
-            mapView.map?.initialViewpoint = AGSViewpoint(targetExtent: AGSEnvelope(xMin: -9812691.11079696, yMin: 5128687.20710657, xMax: -9812377.9447607, yMax: 5128865.36767282, spatialReference: .webMercator()))
+            if let subtype = subtypeFeatureLayer {
+                subtype.load(completion: {[weak self](_: Error?) in
+                    let subtypeSublayer = subtype.sublayer(withName: "Street Light")
+                    subtypeSublayer?.labelsEnabled = true
+                    // subtypeSublayer?.labelDefinitions.
+                    // still have to get labels?
+                    
+                    let originalRednerer = subtypeSublayer?.renderer
+                    
+                    let symbol = AGSSimpleMarkerSymbol(style: .diamond, color: .systemPink, size: 20)
+                    let alternativeRenderer = AGSSimpleRenderer(symbol: symbol)
+                })
+            }
         }
     }
 
+    @IBAction func showSublayer(_ sender: UISwitch) {
+        if sender.isOn {
+            subtypeFeatureLayer?.isVisible = true
+        }
+        else {
+            subtypeFeatureLayer?.isVisible = false
+        }
+    }
+    
+    @IBOutlet private weak var mapView: AGSMapView! {
+        didSet {
+            // initialize the map
+            mapView.map = AGSMap(basemap: .streetsNightVector())
+            mapView.map?.initialViewpoint = AGSViewpoint(targetExtent: AGSEnvelope(xMin: -9812691.11079696, yMin: 5128687.20710657, xMax: -9812377.9447607, yMax: 5128865.36767282, spatialReference: .webMercator()))
+            
+            // on any navigation on the map view
+
+            // create a subtype feature layer from a service feature table
+            let featureServiceURL = URL(string:"https://sampleserver7.arcgisonline.com/arcgis/rest/services/UtilityNetwork/NapervilleElectric/FeatureServer/100")
+            let featureTable = AGSServiceFeatureTable(url: featureServiceURL!)
+            subtypeFeatureLayer = AGSSubtypeFeatureLayer(featureTable: featureTable)
+            mapView.map?.operationalLayers.add(subtypeFeatureLayer as Any)
+            
+        }
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+           super.viewDidLoad()
+           //add the source code button item to the right of navigation bar
+           (self.navigationItem.rightBarButtonItem as! SourceCodeBarButtonItem).filenames = ["DisplaySubtypeFeatureLayer"]
+   }
+}
+
+extension DisplaySubtypeFeatureLayerViewController: UIAdaptivePresentationControllerDelegate {
+    func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+        return .none
+    }
 }

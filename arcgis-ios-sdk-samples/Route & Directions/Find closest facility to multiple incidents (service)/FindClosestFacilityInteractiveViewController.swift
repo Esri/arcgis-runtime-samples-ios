544dc929350adfc5eddd32aa22160418a6b6c0d0
Vivian Quach <viv10382@esri.com>
2020-02-13 13:52:56 -0800

    Updated swift file

--- arcgis-ios-sdk-samples/Route & Directions/Find closest facility to multiple incidents (service)/FindClosestFacilityInteractiveViewController.swift
+++ arcgis-ios-sdk-samples/Route & Directions/Find closest facility to multiple incidents (service)/FindClosestFacilityInteractiveViewController.swift
@@ -32,22 +32,29 @@ class FindClosestFacilityInteractiveViewController: UIViewController {
     private let facilityURL = URL(string: "https://static.arcgis.com/images/Symbols/SafetyHealth/Hospital.png")!
     private var facilityGraphicsOverlay = AGSGraphicsOverlay()
     private var incidentGraphicsOverlay = AGSGraphicsOverlay()
+    let closestFacilityTask: AGSClosestFacilityTask = {
+        let networkServiceURL = URL(string: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/NetworkAnalysis/SanDiego/NAServer/ClosestFacility")!
+        return AGSClosestFacilityTask(url: networkServiceURL)
+    }()
+    private var facilities = [AGSFacility]()
+    let routeSymbol = AGSSimpleLineSymbol(style: .solid, color: .blue, width: 2.0)
+    
     
     private func createFacilitiesAndGraphics() {
-        let facilities = [
-            AGSPoint(x: -1.3042129900625112E7, y: 3860127.9479775648, spatialReference: .webMercator()),
-            AGSPoint(x: -1.3042193400557665E7, y: 3862448.873041752, spatialReference: .webMercator()),
-            AGSPoint(x: -1.3046882875518233E7, y: 3862704.9896770366, spatialReference: .webMercator()),
-            AGSPoint(x: -1.3040539754780494E7, y: 3862924.5938606677, spatialReference: .webMercator()),
-            AGSPoint(x: -1.3042571225655518E7, y: 3858981.773018156, spatialReference: .webMercator()),
-            AGSPoint(x: -1.3039784633928463E7, y: 3856692.5980474586, spatialReference: .webMercator()),
-            AGSPoint(x: -1.3049023883956768E7, y: 3861993.789732541, spatialReference: .webMercator())]
+        facilities = [
+            AGSFacility(point: AGSPoint(x: -1.3042129900625112E7, y: 3860127.9479775648, spatialReference: .webMercator())),
+            AGSFacility(point: AGSPoint(x: -1.3042193400557665E7, y: 3862448.873041752, spatialReference: .webMercator())),
+            AGSFacility(point: AGSPoint(x: -1.3046882875518233E7, y: 3862704.9896770366, spatialReference: .webMercator())),
+            AGSFacility(point: AGSPoint(x: -1.3040539754780494E7, y: 3862924.5938606677, spatialReference: .webMercator())),
+            AGSFacility(point: AGSPoint(x: -1.3042571225655518E7, y: 3858981.773018156, spatialReference: .webMercator())),
+            AGSFacility(point: AGSPoint(x: -1.3039784633928463E7, y: 3856692.5980474586, spatialReference: .webMercator())),
+            AGSFacility(point: AGSPoint(x: -1.3049023883956768E7, y: 3861993.789732541, spatialReference: .webMercator()))]
         let facilitySymbol = AGSPictureMarkerSymbol(url: facilityURL)
         facilitySymbol.height = 30
         facilitySymbol.width = 30
         
         for eachFacility in facilities {
-            facilityGraphicsOverlay.graphics.add(AGSGraphic(geometry: eachFacility, symbol: facilitySymbol, attributes: .none))
+            facilityGraphicsOverlay.graphics.add(AGSGraphic(geometry: eachFacility.geometry, symbol: facilitySymbol, attributes: .none))
         }
     }
     
@@ -61,5 +68,33 @@ class FindClosestFacilityInteractiveViewController: UIViewController {
 
 // MARK: - AGSGeoViewTouchDelegate
 extension FindClosestFacilityInteractiveViewController: AGSGeoViewTouchDelegate {
-    
+    func geoView(_ geoView: AGSGeoView, didTapAtScreenPoint screenPoint: CGPoint, mapPoint: AGSPoint) {
+        closestFacilityTask.defaultClosestFacilityParameters { [weak self] (parameters, error) in
+            guard let self = self else { return }
+            if let parameters = parameters {
+                parameters.setFacilities(self.facilities)
+                let incidentSymbol = AGSSimpleMarkerSymbol(style: .cross, color: .black, size: 20)
+                
+                self.incidentGraphicsOverlay.graphics.removeAllObjects()
+                let incidentPoint = AGSPoint(x: mapPoint.x, y: mapPoint.y, spatialReference: .webMercator())
+                let graphic = AGSGraphic(geometry: incidentPoint, symbol: incidentSymbol, attributes: .none)
+                self.incidentGraphicsOverlay.graphics.add(graphic)
+                
+                parameters.setIncidents([AGSIncident(point: incidentPoint)])
+                self.closestFacilityTask.solveClosestFacility(with: parameters) { [weak self] (result, error) in
+                    guard let self = self else { return }
+                    if let result = result {
+                        let rankedList = result.rankedFacilityIndexes(forIncidentIndex: 0)
+                        let closestFacility = Int(truncating: (rankedList?[0])!)
+                        let route = result.route(forFacilityIndex: closestFacility, incidentIndex: 0)
+                        self.incidentGraphicsOverlay.graphics.add(AGSGraphic(geometry: route?.routeGeometry, symbol: self.routeSymbol, attributes: .none))
+                    } else if let error = error {
+                        self.presentAlert(error: error)
+                    }
+                }
+            } else if let error = error {
+                self.presentAlert(error: error)
+            }
+        }
+    }
 }

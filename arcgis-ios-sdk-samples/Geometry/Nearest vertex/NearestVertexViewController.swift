f9c6281302c4da44c98996b552d2ce2ff45bda74
Ting Chen <tchen@esri.com>
2020-03-12 14:59:19 -0700

    Add the basic setup for any example - a VC and a SB.

--- arcgis-ios-sdk-samples/Geometry/Nearest vertex/NearestVertexViewController.swift
+++ arcgis-ios-sdk-samples/Geometry/Nearest vertex/NearestVertexViewController.swift
@@ -1,9 +1,42 @@
+// Copyright 2020 Esri
 //
-//  NearestVertexViewController.swift
-//  ArcGIS Runtime SDK Samples
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by Ting Chen on 3/12/20.
-//  Copyright © 2020 Esri. All rights reserved.
+//   http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-import Foundation
+import UIKit
+import ArcGIS
+
+class NearestVertexViewController: UIViewController {
+    
+    /// The map view managed by the view controller.
+    @IBOutlet weak var mapView: AGSMapView! {
+        didSet {
+            mapView.map = makeMap()
+            mapView.touchDelegate = self
+        }
+    }
+    
+    /// Creates a map.
+    ///
+    /// - Returns: A new `AGSMap` object.
+    func makeMap() -> AGSMap {
+        let map = AGSMap(basemap: .topographic())
+        return map
+    }
+}
+
+extension NearestVertexViewController: AGSGeoViewTouchDelegate {
+    // MARK: - AGSGeoViewTouchDelegate
+    func geoView(_ geoView: AGSGeoView, didTapAtScreenPoint screenPoint: CGPoint, mapPoint: AGSPoint) {
+        
+    }
+}

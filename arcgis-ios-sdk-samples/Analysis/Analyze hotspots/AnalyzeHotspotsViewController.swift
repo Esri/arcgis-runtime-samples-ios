76765846bc38f9db67b48703b7557765dcf0df1a
Vivian Quach <vquach@esri.com>
2020-04-08 13:57:23 -0700

    Reversed hunks

--- arcgis-ios-sdk-samples/Analysis/Analyze hotspots/AnalyzeHotspotsViewController.swift
+++ arcgis-ios-sdk-samples/Analysis/Analyze hotspots/AnalyzeHotspotsViewController.swift
@@ -52,10 +52,6 @@ class AnalyzeHotspotsViewController: UIViewController, HotspotSettingsVCDelegate
         mapView.map = map
     }
     
-    fileprivate func extractedFunc() -> AGSGeoprocessingParameters {
-        return AGSGeoprocessingParameters(executionType: .asynchronousSubmit)
-    }
-    
     private func analyzeHotspots(_ fromDate: Date, toDate: Date) {
         //cancel previous job request
         self.geoprocessingJob?.progress.cancel()
@@ -64,7 +60,7 @@ class AnalyzeHotspotsViewController: UIViewController, HotspotSettingsVCDelegate
         let toDateString = dateFormatter.string(from: toDate)
         
         //parameters
-        let params = extractedFunc()
+        let params = AGSGeoprocessingParameters(executionType: .asynchronousSubmit)
         params.processSpatialReference = self.mapView.map?.spatialReference
         params.outputSpatialReference = self.mapView.map?.spatialReference
         

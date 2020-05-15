7e342cd9a021b8548da6357953a6cb6bc6a874c7
Ting Chen <tchen@esri.com>
2020-05-15 14:51:53 -0700

    Update source code for new gesture.

--- arcgis-ios-sdk-samples/Layers/Identify raster cell/IdentifyRasterCellViewController.swift
+++ arcgis-ios-sdk-samples/Layers/Identify raster cell/IdentifyRasterCellViewController.swift
@@ -26,8 +26,12 @@ class IdentifyRasterCellViewController: UIViewController {
         didSet {
             mapView.map = makeMap()
             mapView.touchDelegate = self
+            mapView.callout.customView = calloutStackView
+            mapView.callout.isAccessoryButtonHidden = true
         }
     }
+    
+    // MARK: Properties
     
     /// The raster layer created using local raster file.
     let rasterLayer = AGSRasterLayer(raster: AGSRaster(name: "SA_EVI_8Day_03May20", extension: "tif"))
@@ -36,14 +40,18 @@ class IdentifyRasterCellViewController: UIViewController {
     
     /// Create a map.
     ///
-    /// - Parameter layers: The feature layers for the utility network.
     /// - Returns: An `AGSMap` object.
     func makeMap() -> AGSMap {
-        let map = AGSMap(basemapType: .oceans, latitude: -33.9, longitude: 18.6, levelOfDetail: 9)
+        let map = AGSMap(basemapType: .oceans, latitude: -34.1, longitude: 18.6, levelOfDetail: 9)
         map.operationalLayers.add(rasterLayer)
         return map
     }
     
+    /// Identify a pixel on the raster layer and display the result in a callout.
+    ///
+    /// - Parameters:
+    ///   - screenPoint: The point at which to identify features.
+    ///   - offset: An offset to the point to identify to display the callout.
     func identifyPixel(at screenPoint: CGPoint, offset: CGPoint = .zero) {
         mapView.identifyLayer(rasterLayer, screenPoint: screenPoint, tolerance: 1.0, returnPopupsOnly: false) { [weak self] identifyResult in
             guard let self = self else { return }
@@ -55,17 +63,19 @@ class IdentifyRasterCellViewController: UIViewController {
             var calloutText: String = ""
             var xyCoordinates: String = ""
             identifyResult.geoElements.filter { $0 is AGSRasterCell }.forEach { cell in
-                cell.attributes.forEach { attr in
-                    calloutText.append("\(attr.key): \(attr.value)\n")
+                cell.attributes.forEach { attribute in
+                    calloutText.append("\(attribute.key): \(attribute.value)\n")
                 }
-                xyCoordinates = String(format: "X: %.3f\nY: %.3f", cell.geometry!.extent.xMin, cell.geometry!.extent.yMin)
+                xyCoordinates.append(String(format: "X: %.3f\nY: %.3f", cell.geometry!.extent.xMin, cell.geometry!.extent.yMin))
             }
-            let customCallout = self.calloutStackView!
-            customCallout.attributesLabel.text = calloutText
-            customCallout.coordinatesLabel.text = xyCoordinates
-            self.mapView.callout.customView = customCallout
-            self.mapView.callout.isAccessoryButtonHidden = true
-            self.mapView.callout.show(at: self.mapView.screen(toLocation: screenPoint), screenOffset: offset, rotateOffsetWithMap: false, animated: false)
+            self.calloutStackView.attributesLabel.text = calloutText
+            self.calloutStackView.coordinatesLabel.text = xyCoordinates
+            self.mapView.callout.show(
+                at: self.mapView.screen(toLocation: screenPoint),
+                screenOffset: offset,
+                rotateOffsetWithMap: false,
+                animated: false
+            )
         }
     }
     
@@ -75,29 +85,34 @@ class IdentifyRasterCellViewController: UIViewController {
         super.viewDidLoad()
         // Add the source code button item to the right of navigation bar.
         (self.navigationItem.rightBarButtonItem as? SourceCodeBarButtonItem)?.filenames = ["IdentifyRasterCellViewController"]
-        // Set map view's viewpoint to the raster layer's full extent
+        // Load the raster layer.
         rasterLayer.load { [weak self] (error) in
-            guard let self = self else { return }
-            if let center = self.rasterLayer.fullExtent?.center {
-                self.mapView.setViewpointCenter(center)
-            } else if let error = error {
-                self.presentAlert(error: error)
+            if let error = error {
+                self?.presentAlert(error: error)
             }
         }
     }
 }
 
+// MARK: - AGSGeoViewTouchDelegate
+
 extension IdentifyRasterCellViewController: AGSGeoViewTouchDelegate {
-    func geoView(_ geoView: AGSGeoView, didTouchDownAtScreenPoint screenPoint: CGPoint, mapPoint: AGSPoint, completion: @escaping (Bool) -> Void) {
-        // Tell the ArcGIS Runtime if we are going to handle interaction.
-        completion(true)
-    }
-    
-    func geoView(_ geoView: AGSGeoView, didTouchUpAtScreenPoint screenPoint: CGPoint, mapPoint: AGSPoint) {
+    func geoView(_ geoView: AGSGeoView, didTapAtScreenPoint screenPoint: CGPoint, mapPoint: AGSPoint) {
+        // Tap to identify a pixel on the raster layer.
         identifyPixel(at: screenPoint)
     }
     
-    func geoView(_ geoView: AGSGeoView, didTouchDragToScreenPoint screenPoint: CGPoint, mapPoint: AGSPoint) {
-        identifyPixel(at: screenPoint, offset: CGPoint(x: 0, y: -50))
+    func geoView(_ geoView: AGSGeoView, didLongPressAtScreenPoint screenPoint: CGPoint, mapPoint: AGSPoint) {
+        // Show the callout with offset so that the finger does not cover the callout.
+        identifyPixel(at: screenPoint, offset: CGPoint(x: 0, y: -70))
+    }
+    
+    func geoView(_ geoView: AGSGeoView, didMoveLongPressToScreenPoint screenPoint: CGPoint, mapPoint: AGSPoint) {
+        identifyPixel(at: screenPoint, offset: CGPoint(x: 0, y: -70))
+    }
+    
+    func geoView(_ geoView: AGSGeoView, didEndLongPressAtScreenPoint screenPoint: CGPoint, mapPoint: AGSPoint) {
+        // When tap drag finishes, show the callout without offset.
+        identifyPixel(at: screenPoint)
     }
 }

f5ea6b9153cfb051181ce609730ef9eb48b6006c
Vivian Quach <vquach@esri.com>
2020-06-17 16:15:37 -0700

    Added popup delegate

--- arcgis-ios-sdk-samples/Display information/Show a popup/ShowPopupViewController.swift
+++ arcgis-ios-sdk-samples/Display information/Show a popup/ShowPopupViewController.swift
@@ -15,12 +15,10 @@
 import UIKit
 import ArcGIS
 
-class ShowPopupViewController: UIViewController, AGSGeoViewTouchDelegate {
+class ShowPopupViewController: UIViewController, AGSGeoViewTouchDelegate, AGSPopupsViewControllerDelegate {
     @IBOutlet private weak var mapView: AGSMapView! {
         didSet {
             mapView.map = makeMap()
-            //set touch delegate
-            mapView.touchDelegate = self
         }
     }
     
@@ -30,37 +28,47 @@ class ShowPopupViewController: UIViewController, AGSGeoViewTouchDelegate {
     func makeMap() -> AGSMap {
         let mapURL = URL(string: "https://runtime.maps.arcgis.com/home/item.html?id=ccf828425b5a456b90eb75cf72278e13")!
         let map = AGSMap(url: mapURL)
-        map?.initialViewpoint = AGSViewpoint(latitude: 47.607042, longitude: -122.324304, scale: 15)
-//        let featureServiceURL = URL(string: "https://services2.arcgis.com/ZQgQTuoyBrtmoGdP/arcgis/rest/services/Seattle_Downtown_Features/FeatureServer/4")!
-//        let featureTable = AGSServiceFeatureTable(url: featureServiceURL)
-//        featureLayer = AGSFeatureLayer(featureTable: featureTable)
-//        map?.operationalLayers.add(featureLayer!)
-        let b = map?.operationalLayers[0] as! AGSLayer
-        print(b.name)
+        map?.initialViewpoint = AGSViewpoint(latitude: 47.6062, longitude: -122.3321, scale: 2e4)
+        map?.load { [weak self] (error) in
+        guard let self = self else { return }
+            if let error = error {
+                self.presentAlert(error: error)
+            } else {
+                //set touch delegate
+                self.mapView.touchDelegate = self
+                self.featureLayer = map?.operationalLayers[0] as? AGSFeatureLayer
+            }
+        }
         return map!
     }
     
     // MARK: - AGSGeoViewTouchDelegate
     func geoView(_ geoView: AGSGeoView, didTapAtScreenPoint screenPoint: CGPoint, mapPoint: AGSPoint) {
         self.mapView.identifyLayer(featureLayer!, screenPoint: screenPoint, tolerance: 12, returnPopupsOnly: false) { [weak self] (result: AGSIdentifyLayerResult) in
+            guard let self = self else { return }
             if let error = result.error {
-                self?.presentAlert(error: error)
+                self.presentAlert(error: error)
             } else {
-                if result.popups.isEmpty {
-                    print("EMPTY")
-                }
-                self?.popupsViewController = AGSPopupsViewController(popups: result.popups)
-//                self?.popupsViewController?.delegate = self
-                
-                self!.present(self!.popupsViewController!, animated: true)
+                self.popupsViewController = AGSPopupsViewController(popups: result.popups)
+                self.popupsViewController?.modalPresentationStyle = .formSheet
+                self.present(self.popupsViewController!, animated: true)
+                self.popupsViewController?.delegate = self
             }
         }
+    }
+    
+    // MARK: - AGSPopupsViewControllerDelegate methods
+    func popupsViewControllerDidFinishViewingPopups(_ popupsViewController: AGSPopupsViewController) {
+        // Dismiss the popups view controller.
+        self.dismiss(animated: true)
+        // Reset the popups view controller
+        self.popupsViewController = nil
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //add the source code button item to the right of navigation bar
+        // Add the source code button item to the right of navigation bar.
         (self.navigationItem.rightBarButtonItem as! SourceCodeBarButtonItem).filenames = ["ShowPopupViewController"]
     }
 }

6c636ac197d0696fc169928aa7fe902072cca49d
Vivian Quach <vquach@esri.com>
2020-06-17 17:13:25 -0700

    Added README, image, and comments

--- arcgis-ios-sdk-samples/Display information/Show a popup/ShowPopupViewController.swift
+++ arcgis-ios-sdk-samples/Display information/Show a popup/ShowPopupViewController.swift
@@ -26,16 +26,20 @@ class ShowPopupViewController: UIViewController, AGSGeoViewTouchDelegate, AGSPop
     var popupsViewController: AGSPopupsViewController?
     
     func makeMap() -> AGSMap {
+        // Create a map using a URL.
         let mapURL = URL(string: "https://runtime.maps.arcgis.com/home/item.html?id=ccf828425b5a456b90eb75cf72278e13")!
         let map = AGSMap(url: mapURL)
+        // Set the initial viewpoint to Downtown Seattle.
         map?.initialViewpoint = AGSViewpoint(latitude: 47.6062, longitude: -122.3321, scale: 2e4)
+        // Load the map.
         map?.load { [weak self] (error) in
         guard let self = self else { return }
             if let error = error {
                 self.presentAlert(error: error)
             } else {
-                //set touch delegate
+                // Set touch delegate.
                 self.mapView.touchDelegate = self
+                // Get the feature layer.
                 self.featureLayer = map?.operationalLayers[0] as? AGSFeatureLayer
             }
         }
@@ -44,13 +48,17 @@ class ShowPopupViewController: UIViewController, AGSGeoViewTouchDelegate, AGSPop
     
     // MARK: - AGSGeoViewTouchDelegate
     func geoView(_ geoView: AGSGeoView, didTapAtScreenPoint screenPoint: CGPoint, mapPoint: AGSPoint) {
+        // Identify the specified feature layer.
         self.mapView.identifyLayer(featureLayer!, screenPoint: screenPoint, tolerance: 12, returnPopupsOnly: false) { [weak self] (result: AGSIdentifyLayerResult) in
             guard let self = self else { return }
             if let error = result.error {
                 self.presentAlert(error: error)
-            } else {
+            } else if !result.popups.isEmpty {
+                // Display a popup only if it exists.
                 self.popupsViewController = AGSPopupsViewController(popups: result.popups)
+                // Display the popup as a formsheet -- specified for iPads.
                 self.popupsViewController?.modalPresentationStyle = .formSheet
+                // Present the popup.
                 self.present(self.popupsViewController!, animated: true)
                 self.popupsViewController?.delegate = self
             }

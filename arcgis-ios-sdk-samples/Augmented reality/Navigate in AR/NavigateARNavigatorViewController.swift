bd2159efe66da2134193896bec1efe413fb87f78
Ting Chen <tchen@esri.com>
2020-06-19 16:34:32 -0700

    Change clipping distance to 400 meters.

--- arcgis-ios-sdk-samples/Augmented reality/Navigate in AR/NavigateARNavigatorViewController.swift
+++ arcgis-ios-sdk-samples/Augmented reality/Navigate in AR/NavigateARNavigatorViewController.swift
@@ -38,8 +38,8 @@ class NavigateARNavigatorViewController: UIViewController {
             sceneView.spaceEffect = .transparent
             sceneView.atmosphereEffect = .none
             arView.locationDataSource = AGSCLLocationDataSource()
-            // Clip the navigation tube in 200 meters, to avoid the tube blocking camera.
-            arView.clippingDistance = 200
+            // Clip the navigation tube in 400 meters, to avoid the tube blocking camera.
+            arView.clippingDistance = 400
         }
     }
     

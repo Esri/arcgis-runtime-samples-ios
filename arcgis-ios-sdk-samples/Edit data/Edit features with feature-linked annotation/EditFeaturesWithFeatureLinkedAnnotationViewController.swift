b140492d8b87e77b2e04fba90607d8385d1f16c3
Vivian Quach <vquach@esri.com>
2020-07-28 12:43:35 -0700

    Added loudoun_anno geodatabase

--- arcgis-ios-sdk-samples/Edit data/Edit features with feature-linked annotation/EditFeaturesWithFeatureLinkedAnnotationViewController.swift
+++ arcgis-ios-sdk-samples/Edit data/Edit features with feature-linked annotation/EditFeaturesWithFeatureLinkedAnnotationViewController.swift
@@ -1,9 +1,33 @@
 //
-//  EditFeaturesWithFeatureLinkedAnnotationViewController.swift
-//  ArcGIS Runtime SDK Samples
+// Copyright © 2020 Esri.
 //
-//  Created by Vivian Quach on 7/27/20.
-//  Copyright © 2020 Esri. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
-import Foundation
+import UIKit
+import ArcGIS
+
+class EditFeaturesWithFeatureLinkedAnnotationViewController: UIViewController {
+    @IBOutlet weak var mapView: AGSMapView! {
+        didSet {
+            
+        }
+    }
+    
+    func makeMap() -> AGSMap {
+        let geodatabase = AGSGeodatabase(fileURL: <#T##URL#>)
+        let map = AGSMap(basemapType: .lightGrayCanvasVector, latitude: 39.0204, longitude: -77.4159, levelOfDetail: 18)
+        map.operationalLayers.add(<#T##anObject: Any##Any#>)
+        return map
+    }
+}

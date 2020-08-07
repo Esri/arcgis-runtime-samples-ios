b1de5c76e9ac0def94c88ebf301836d557b391d3
Ting Chen <tchen@esri.com>
2020-08-07 15:24:55 -0700

    Remove nav bar tint from code as it got override.

--- arcgis-ios-sdk-samples/AppDelegate.swift
+++ arcgis-ios-sdk-samples/AppDelegate.swift
@@ -116,7 +116,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
             navigationBarAppearanceProxy.titleTextAttributes = [.foregroundColor: UIColor.white]
             navigationBarAppearanceProxy.barTintColor = .accentColor
         }
-        navigationBarAppearanceProxy.tintColor = .white
         UISwitch.appearance().onTintColor = .accentColor
         if #available(iOS 14.0, *) {
             // Nothing to do! iOS 14 handles global tint with accent color.

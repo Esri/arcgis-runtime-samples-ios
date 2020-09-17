d02fb7674e5e433408bdff1d1d432da44c7bef95
Ting Chen <tchen@esri.com>
2020-09-17 11:44:46 -0700

    Add source code files.

---
+++ arcgis-ios-sdk-samples/Layers/Raster rendering rule/RasterRenderingRuleViewController.swift
@@ -0,0 +1,104 @@
+// Copyright 2020 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import UIKit
+import ArcGIS
+
+class RasterRenderingRuleViewController: UIViewController {
+    // MARK: Storyboard views
+    
+    /// The map view managed by the view controller.
+    @IBOutlet var mapView: AGSMapView! {
+        didSet {
+            mapView.map = makeMap()
+        }
+    }
+    /// The choose rendering rule button.
+    @IBOutlet var chooseRenderingRuleBarButtonItem: UIBarButtonItem!
+    
+    // MARK: Properties and methods
+    
+    /// An online image service that features Charlotte, North Carolina's downtown area.
+    var imageServiceURL = URL(string: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/CharlotteLAS/ImageServer")!
+    /// A list of rendering rule info supported by the service.
+    var rasterRenderingRules = [AGSRenderingRuleInfo]()
+    
+    /// Create a map.
+    ///
+    /// - Returns: An `AGSMap` object.
+    func makeMap() -> AGSMap {
+        let map = AGSMap(basemap: .streets())
+        // Create a raster layer from the raster and add it to the map.
+        let imageServiceRaster = AGSImageServiceRaster(url: imageServiceURL)
+        map.operationalLayers.add(makeRasterLayer(raster: imageServiceRaster))
+        // Load the raster and get a list of rendering rule info supported by the service.
+        imageServiceRaster.load { [weak self] error in
+            guard let self = self else { return }
+            if let serviceInfo = imageServiceRaster.serviceInfo, let extent = serviceInfo.fullExtent {
+                self.mapView.setViewpoint(AGSViewpoint(targetExtent: extent), completion: nil)
+                self.rasterRenderingRules = serviceInfo.renderingRuleInfos
+                self.chooseRenderingRuleBarButtonItem.isEnabled = true
+            } else if let error = error {
+                self.presentAlert(error: error)
+            }
+        }
+        return map
+    }
+    
+    /// Create a raster layer from an image service raster with an optional rendering rule.
+    ///
+    /// - Parameters:
+    ///   - raster: An `AGSImageServiceRaster` object.
+    ///   - renderingRule: An optional `AGSRenderingRule` object.
+    /// - Returns: An `AGSRasterLayer` object.
+    func makeRasterLayer(raster: AGSImageServiceRaster, renderingRule: AGSRenderingRule? = nil) -> AGSRasterLayer {
+        raster.renderingRule = renderingRule
+        let rasterLayer = AGSRasterLayer(raster: raster)
+        return rasterLayer
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func chooseRenderingRule(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(
+            title: "Choose a raster rendering rule to apply to the image service raster.",
+            message: nil,
+            preferredStyle: .actionSheet
+        )
+        rasterRenderingRules.forEach { ruleInfo in
+            let action = UIAlertAction(title: ruleInfo.name, style: .default) { _ in
+                // Clear all raster layers before adding new one.
+                self.mapView.map!.operationalLayers.removeAllObjects()
+                // Create a new `AGSRenderingRule` object with the chosen rule.
+                let renderingRule = AGSRenderingRule(renderingRuleInfo: ruleInfo)
+                let imageServiceRaster = AGSImageServiceRaster(url: self.imageServiceURL)
+                // Add the new raster layer to the map.
+                self.mapView.map!.operationalLayers.add(self.makeRasterLayer(raster: imageServiceRaster, renderingRule: renderingRule))
+            }
+            alertController.addAction(action)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertController.addAction(cancelAction)
+        alertController.popoverPresentationController?.barButtonItem = sender
+        present(alertController, animated: true)
+    }
+    
+    // MARK: UIViewController
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Add the source code button item to the right of navigation bar.
+        (navigationItem.rightBarButtonItem as? SourceCodeBarButtonItem)?.filenames = ["RasterRenderingRuleViewController"]
+    }
+}

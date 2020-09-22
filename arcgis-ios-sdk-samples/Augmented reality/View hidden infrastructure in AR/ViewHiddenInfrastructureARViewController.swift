6bed2ef6edd57c5d1fd715994eb732b4a82f32bc
Ting Chen <tchen@esri.com>
2020-09-22 16:01:23 -0700

    Add info to build on device.

--- arcgis-ios-sdk-samples/Augmented reality/View hidden infrastructure in AR/ViewHiddenInfrastructureARViewController.swift
+++ arcgis-ios-sdk-samples/Augmented reality/View hidden infrastructure in AR/ViewHiddenInfrastructureARViewController.swift
@@ -37,7 +37,7 @@ class ViewHiddenInfrastructureARPipePlacer: UIViewController {
                                           action: #selector(startExperience(_:)))
     private let elevationSlider = UISlider()
     private let helpLabel = UILabel()
-
+    
     // Map and data
     private let map = AGSMap(basemap: .imageryWithLabelsVector())
     let elevationSource = AGSArcGISTiledElevationSource(url:
@@ -47,27 +47,27 @@ class ViewHiddenInfrastructureARPipePlacer: UIViewController {
     private let locationDataSource = AGSCLLocationDataSource()
     private let pipesOverlay = AGSGraphicsOverlay()
     private var hasInitialLocation = false
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         mapView.translatesAutoresizingMaskIntoConstraints = false
-
+        
         helpLabel.translatesAutoresizingMaskIntoConstraints = false
         helpLabel.text = "Sketch pipes to visualize in AR"
         helpLabel.textAlignment = .center
         helpLabel.textColor = .white
         helpLabel.backgroundColor = UIColor(white: 0, alpha: 0.6)
-
+        
         let elevationButton = UIBarButtonItem(customView: elevationSlider)
         elevationSlider.minimumValue = -10
         elevationSlider.maximumValue = 10
-
+        
         viewBBI.isEnabled = false
         addBBI.isEnabled = false
         undoBBI.isEnabled = false
         redoBBI.isEnabled = false
-
+        
         toolbar.translatesAutoresizingMaskIntoConstraints = false
         toolbar.items = [
             addBBI,
@@ -81,7 +81,7 @@ class ViewHiddenInfrastructureARPipePlacer: UIViewController {
         view.addSubview(mapView)
         view.addSubview(toolbar)
         view.addSubview(helpLabel)
-
+        
         NSLayoutConstraint.activate([
             mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             mapView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
@@ -94,19 +94,18 @@ class ViewHiddenInfrastructureARPipePlacer: UIViewController {
             helpLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             helpLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             helpLabel.heightAnchor.constraint(equalToConstant: 40)
-            ])
-
+        ])
+        
         configureMap()
     }
-
+    
     private func configureMap() {
         mapView.map = map
         mapView.sketchEditor = sketchEditor
-
+        
         // Configure the graphics overlay for showing the pipes
         mapView.graphicsOverlays.add(pipesOverlay)
-        pipesOverlay.renderer = AGSSimpleRenderer(symbol: AGSSimpleLineSymbol(style: .solid,
-                                                                                 color: .red, width: 2))
+        pipesOverlay.renderer = AGSSimpleRenderer(symbol: AGSSimpleLineSymbol(style: .solid, color: .red, width: 2))
         // Configure location display
         locationDataSource.locationChangeHandlerDelegate = self
         locationDataSource.start { [weak self] (err: Error?) in
@@ -115,13 +114,13 @@ class ViewHiddenInfrastructureARPipePlacer: UIViewController {
                 self.presentAlert(error: error)
             }
         }
-
+        
         // Stop the location updates upon user interaction
         mapView.touchDelegate = self
-
+        
         // Configure the elevation surface used to place drawn graphics relative to the ground
         elevationSurface.elevationSources.append(elevationSource)
-        elevationSource.load {[weak self](err: Error?) in
+        elevationSource.load { [weak self](err: Error?) in
             guard let self = self else { return }
             if let error = err {
                 self.presentAlert(error: error)
@@ -130,8 +129,9 @@ class ViewHiddenInfrastructureARPipePlacer: UIViewController {
             }
         }
     }
-
-    @objc func startExperience(_ sender: UIBarButtonItem) {
+    
+    @objc
+    func startExperience(_ sender: UIBarButtonItem) {
         if let graphicsArray = pipesOverlay.graphics as? [AGSGraphic] {
             let viewerVC = HiddenInfrastructureARViewer(pipeGraphics: graphicsArray)
             navigationController?.pushViewController(viewerVC, animated: true)
@@ -144,34 +144,38 @@ class ViewHiddenInfrastructureARPipePlacer: UIViewController {
 // MARK: - sketching infrastructure graphics
 
 extension ViewHiddenInfrastructureARPipePlacer {
-    @objc func respondToGeomChanged() {
+    @objc
+    func respondToGeomChanged() {
         //Enable/disable UI elements appropriately
         undoBBI.isEnabled = self.sketchEditor.undoManager.canUndo
         redoBBI.isEnabled = self.sketchEditor.undoManager.canRedo
     }
-
-    @objc func undo(_ sender: UIBarButtonItem) {
+    
+    @objc
+    func undo(_ sender: UIBarButtonItem) {
         if sketchEditor.undoManager.canUndo { //extra check, just to be sure
             sketchEditor.undoManager.undo()
         }
         respondToGeomChanged()
     }
-
-    @objc func redo(_ sender: UIBarButtonItem) {
+    
+    @objc
+    func redo(_ sender: UIBarButtonItem) {
         if sketchEditor.undoManager.canRedo { //extra check, just to be sure
             sketchEditor.undoManager.redo()
         }
         respondToGeomChanged()
     }
-
-    @objc func startSketch(_ sender: UIBarButtonItem) {
+    
+    @objc
+    func startSketch(_ sender: UIBarButtonItem) {
         let elevationOffset = (Double)(elevationSlider.value)
         if let geometry = sketchEditor.geometry as? AGSPolyline {
             guard let firstpoint = geometry.parts.array().first?.startPoint else { return }
-
+            
             elevationSurface.elevation(for: firstpoint) { [weak self] (elevation: Double, err: Error?) in
                 guard let self = self else { return }
-
+                
                 if let error = err {
                     let graphic = AGSGraphic(geometry: geometry, symbol: nil, attributes: nil)
                     self.pipesOverlay.graphics.add(graphic)
@@ -184,7 +188,7 @@ extension ViewHiddenInfrastructureARPipePlacer {
                     let graphic = AGSGraphic(geometry: elevatedGeometry, symbol: nil, attributes: nil)
                     self.pipesOverlay.graphics.add(graphic)
                     self.viewBBI.isEnabled = true
-
+                    
                     if elevationOffset < 0 {
                         self.helpLabel.text = String(format: "Pipe added %.2fm below surface", elevationOffset * -1)
                     } else if elevationOffset == 0 {
@@ -206,7 +210,7 @@ extension ViewHiddenInfrastructureARPipePlacer: AGSLocationChangeHandlerDelegate
                                    locationDidChange location: AGSLocation) {
         let newViewpoint = AGSViewpoint(center: location.position!, scale: 1000)
         self.mapView.setViewpoint(newViewpoint)
-
+        
         if location.horizontalAccuracy < 20 {
             locationDataSource.locationChangeHandlerDelegate = nil
             locationDataSource.stop()
@@ -215,8 +219,7 @@ extension ViewHiddenInfrastructureARPipePlacer: AGSLocationChangeHandlerDelegate
 }
 
 extension ViewHiddenInfrastructureARPipePlacer: AGSGeoViewTouchDelegate {
-    func geoView(_ geoView: AGSGeoView, didTouchDownAtScreenPoint screenPoint: CGPoint,
-                 mapPoint: AGSPoint, completion: @escaping (Bool) -> Void) {
+    func geoView(_ geoView: AGSGeoView, didTouchDownAtScreenPoint screenPoint: CGPoint, mapPoint: AGSPoint, completion: @escaping (Bool) -> Void) {
         // Stop updating the location
         locationDataSource.locationChangeHandlerDelegate = nil
         locationDataSource.stop()
@@ -226,7 +229,7 @@ extension ViewHiddenInfrastructureARPipePlacer: AGSGeoViewTouchDelegate {
 
 class HiddenInfrastructureARViewer: UIViewController, ARSCNViewDelegate {
     // UI controls
-    public let arView = ArcGISARView()
+    let arView = ArcGISARView()
     private let toolbar = UIToolbar()
     private let helpLabel = UILabel()
     private let arKitStatusLabel = UILabel()
@@ -240,7 +243,7 @@ class HiddenInfrastructureARViewer: UIViewController, ARSCNViewDelegate {
     private let scene = AGSScene(basemapType: .imageryWithLabels)
     private var pipeGraphics: [AGSGraphic]
     private let infrastructureOverlay = AGSGraphicsOverlay()
-
+    
     private var isCalibrating: Bool {
         didSet {
             if isCalibrating {
@@ -252,39 +255,39 @@ class HiddenInfrastructureARViewer: UIViewController, ARSCNViewDelegate {
             }
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         toolbar.translatesAutoresizingMaskIntoConstraints = false
         helpLabel.translatesAutoresizingMaskIntoConstraints = false
         arKitStatusLabel.translatesAutoresizingMaskIntoConstraints = false
         arView.translatesAutoresizingMaskIntoConstraints = false
-
+        
         view.addSubview(arView)
         view.addSubview(helpLabel)
         view.addSubview(arKitStatusLabel)
         view.addSubview(toolbar)
-
+        
         toolbar.items = [
             calibrationBBI,
             UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil),
             UIBarButtonItem(customView: realScaleModePicker)
         ]
-
+        
         realScaleModePicker.selectedSegmentIndex = 0
         realScaleModePicker.addTarget(self, action: #selector(setRealScaleMode(_:)), for: UIControl.Event.valueChanged)
-
+        
         helpLabel.textAlignment = .center
         helpLabel.textColor = .white
         helpLabel.backgroundColor = UIColor(white: 0, alpha: 0.6)
         helpLabel.text = "Tap calibrate to start"
-
+        
         arKitStatusLabel.textAlignment = .center
         arKitStatusLabel.textColor = .black
         arKitStatusLabel.backgroundColor = UIColor(white: 1, alpha: 0.6)
         arKitStatusLabel.text = "Setting up ARKit"
-
+        
         NSLayoutConstraint.activate([
             arView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             arView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
@@ -301,24 +304,24 @@ class HiddenInfrastructureARViewer: UIViewController, ARSCNViewDelegate {
             arKitStatusLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             arKitStatusLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             arKitStatusLabel.heightAnchor.constraint(equalToConstant: 40)
-            ])
-
+        ])
+        
         configureSceneForAR()
     }
-
+    
     private func configureSceneForAR() {
         // Display the scene and set scene view properties
         arView.sceneView.scene = scene
         arView.sceneView.spaceEffect = .transparent
         arView.sceneView.atmosphereEffect = .none
         arView.arSCNViewDelegate = self
-
+        
         // Create an elevation source and add it to the scene
         let elevationSource = AGSArcGISTiledElevationSource(url:
             URL(string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!)
         scene.baseSurface?.elevationSources.append(elevationSource)
         scene.baseSurface?.navigationConstraint = .none
-
+        
         // Configure and add the overlay for showing drawn infrastructure
         let strokeSymbolLayer = AGSSolidStrokeSymbolLayer()
         strokeSymbolLayer.capStyle = .round
@@ -330,31 +333,31 @@ class HiddenInfrastructureARViewer: UIViewController, ARSCNViewDelegate {
         infrastructureOverlay.renderer = polylineRenderer
         infrastructureOverlay.sceneProperties?.surfacePlacement = .absolute
         arView.sceneView.graphicsOverlays.add(infrastructureOverlay)
-
-        let infrastructureGraphics = pipeGraphics.map { AGSGraphic (geometry: $0.geometry, symbol: nil)}
+        
+        let infrastructureGraphics = pipeGraphics.map { AGSGraphic(geometry: $0.geometry, symbol: nil)}
         infrastructureOverlay.graphics.addObjects(from: infrastructureGraphics)
-
+        
         // Configure location data source
         arView.locationDataSource = AGSCLLocationDataSource()
     }
-
-    public init(pipeGraphics: [AGSGraphic]) {
+    
+    init(pipeGraphics: [AGSGraphic]) {
         self.pipeGraphics = pipeGraphics
         calibrationVC = ViewHiddenInfrastructureARCalibrationViewController(arcgisARView: arView)
         isCalibrating = false
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         arView.startTracking(.continuous)
         showCalibrationPopup(calibrationBBI)
     }
-
+    
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         arView.stopTracking()
@@ -364,23 +367,25 @@ class HiddenInfrastructureARViewer: UIViewController, ARSCNViewDelegate {
 // MARK: - Calibration view management
 
 extension HiddenInfrastructureARViewer {
-    @objc func setRealScaleMode(_ sender: UISegmentedControl) {
+    @objc
+    func setRealScaleMode(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0 {
             // Roaming - continuous update
             arView.stopTracking()
             arView.startTracking(.continuous)
-//            arView.startTracking(useLocationDataSourceOnce: false, completion: nil)
+            //            arView.startTracking(useLocationDataSourceOnce: false, completion: nil)
             helpLabel.text = "Using CoreLocation + ARKit"
         } else {
             // Local - only update once, then manually calibrate
             arView.stopTracking()
             arView.startTracking(.initial)
-//            arView.startTracking(useLocationDataSourceOnce: true, completion: nil)
+            //            arView.startTracking(useLocationDataSourceOnce: true, completion: nil)
             helpLabel.text = "Using ARKit only"
         }
     }
-
-    @objc func showCalibrationPopup(_ sender: UIBarButtonItem) {
+    
+    @objc
+    func showCalibrationPopup(_ sender: UIBarButtonItem) {
         if isCalibrating {
             isCalibrating = false
             calibrationVC.dismiss(animated: true, completion: nil)
@@ -390,7 +395,7 @@ extension HiddenInfrastructureARViewer {
             showPopup(controller, sourceButton: sender)
         }
     }
-
+    
     private func showPopup(_ controller: UIViewController, sourceButton: UIBarButtonItem) {
         if let presentationController = AlwaysPresentAsPopover.configurePresentation(forController: controller) {
             presentationController.barButtonItem = sourceButton
@@ -415,15 +420,11 @@ class AlwaysPresentAsPopover: NSObject, UIPopoverPresentationControllerDelegate 
     // Source    https://www.ralfebert.de/ios-examples/uikit/choicepopover/
     // `sharedInstance` because the delegate property is weak - the delegate instance needs to be retained.
     private static let sharedInstance = AlwaysPresentAsPopover()
-
-    private override init() {
-        super.init()
-    }
-
+    
     func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
         return .none
     }
-
+    
     static func configurePresentation(forController controller: UIViewController) -> UIPopoverPresentationController? {
         controller.modalPresentationStyle = .popover
         if let presentationController = controller.presentationController as? UIPopoverPresentationController {
@@ -436,7 +437,7 @@ class AlwaysPresentAsPopover: NSObject, UIPopoverPresentationControllerDelegate 
 
 // MARK: - tracking status display
 extension HiddenInfrastructureARViewer {
-    public func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
         // Don't show anything in roaming mode; constant location tracking reset means
         // ARKit will always be initializing
         if realScaleModePicker.selectedSegmentIndex == 0 {

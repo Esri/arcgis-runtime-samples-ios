267312c897cfbc71a2e673022990b6cc30e1f35d
Vivian Quach <vquach@esri.com>
2020-09-24 16:34:13 -0700

    Made pure function

--- arcgis-ios-sdk-samples/Layers/Edit KML ground overlay/EditKMLGroundOverlayViewController.swift
+++ arcgis-ios-sdk-samples/Layers/Edit KML ground overlay/EditKMLGroundOverlayViewController.swift
@@ -19,7 +19,7 @@ class EditKMLGroundOverlayViewController: UIViewController {
     // MARK: Storyboard views and properties
     @IBOutlet var sceneView: AGSSceneView! {
         didSet {
-            sceneView.scene = makeScene()
+            sceneView.scene = makeScene(groundOverlay: overlay)
         }
     }
     // The slider that controls the overlay's opacity.
@@ -28,7 +28,14 @@ class EditKMLGroundOverlayViewController: UIViewController {
     @IBOutlet var valueLabel: UILabel!
     
     // The KML ground overlay.
-    var overlay: AGSKMLGroundOverlay!
+    let overlay: AGSKMLGroundOverlay = {
+        // Create a geometry for the ground overlay.
+        let overlayGeometry = AGSEnvelope(xMin: -123.066227926904, yMin: 44.04736963555683, xMax: -123.0796942287304, yMax: 44.03878298600624, spatialReference: .wgs84())
+        // Create a KML icon for the overlay image.
+        let imageURL = URL(string: "https://libapps.s3.amazonaws.com/accounts/55937/images/1944.jpg")!
+        let overlayImage = AGSKMLIcon(url: imageURL)
+        return AGSKMLGroundOverlay(geometry: overlayGeometry, icon: overlayImage)!
+    }()
     
     // MARK: Actions and methods
     @IBAction func sliderValueChanged(_ slider: UISlider) {
@@ -36,34 +43,29 @@ class EditKMLGroundOverlayViewController: UIViewController {
         let alpha = CGFloat(slider.value)
         overlay.color = UIColor.black.withAlphaComponent(alpha)
         // Update the slider's value label.
-        let numberFormatter = NumberFormatter()
-        numberFormatter.maximumFractionDigits = 2
+        let numberFormatter: NumberFormatter = {
+            let numberFormatter = NumberFormatter()
+            numberFormatter.maximumFractionDigits = 2
+            return numberFormatter
+        }()
         valueLabel.text = numberFormatter.string(from: slider.value as NSNumber)
     }
     
-    func makeScene() -> AGSScene {
+    func makeScene(groundOverlay: AGSKMLGroundOverlay) -> AGSScene {
         // Create a scene for the scene view.
         let scene = AGSScene(basemap: .imagery())
-        // Create a geometry for the ground overlay.
-        let overlayGeometry = AGSEnvelope(xMin: -123.066227926904, yMin: 44.04736963555683, xMax: -123.0796942287304, yMax: 44.03878298600624, spatialReference: .wgs84())
-        // Create a KML icon for the overlay image.
-        let imageURL = URL(string: "https://libapps.s3.amazonaws.com/accounts/55937/images/1944.jpg")!
-        let overlayImage = AGSKMLIcon(url: imageURL)
-        // Apply a KML ground overlay.
-        let overlay = AGSKMLGroundOverlay(geometry: overlayGeometry, icon: overlayImage)!
-        self.overlay = overlay
         // Set the rotation of the ground overlay.
-        overlay.rotation = -3.046024799346924
+        groundOverlay.rotation = -3.046024799346924
         // Create a KML dataset with the ground overlay as the root node.
-        let dataset = AGSKMLDataset(rootNode: overlay)
+        let dataset = AGSKMLDataset(rootNode: groundOverlay)
         // Create a KML layer for the scene view.
         let layer = AGSKMLLayer(kmlDataset: dataset)
         // Add the layer to the scene.
         scene.operationalLayers.add(layer)
         // Move the viewpoint to the ground overlay.
-        let overlayCenter = overlay.geometry.extent.center
+        let overlayCenter = groundOverlay.geometry.extent.center
         let camera = AGSCamera(lookAt: overlayCenter, distance: 1250, heading: 45, pitch: 60, roll: 0)
-        let targetExtent = overlay.geometry as! AGSEnvelope
+        let targetExtent = groundOverlay.geometry as! AGSEnvelope
         sceneView.setViewpoint(AGSViewpoint(targetExtent: targetExtent, camera: camera))
         // Return the scene.
         return scene

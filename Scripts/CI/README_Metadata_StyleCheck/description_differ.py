0b22f3f59cfc5a0f518b7544f6f5f2bc6f900848
Ting Chen <tchen@esri.com>
2020-10-06 14:32:13 -0700

    Fix comment in code.

--- Scripts/CI/README_Metadata_StyleCheck/description_differ.py
+++ Scripts/CI/README_Metadata_StyleCheck/description_differ.py
@@ -269,8 +269,8 @@ def main():
     msg = 'Description checker script. Run it against the ' \
           '/arcgis-ios-sdk-samples folder or a single sample folder. ' \
           'On success: Script will exit with zero. ' \
-          'On failure: Title inconsistency will print to console and the ' \
-          'script will exit with non-zero code.'
+          'On failure: Description inconsistency will print to console and ' \
+          'the script will exit with non-zero code.'
     parser = argparse.ArgumentParser(description=msg)
     parser.add_argument('-a', '--all', help='path to arcgis-ios-sdk-samples '
                                             'folder')

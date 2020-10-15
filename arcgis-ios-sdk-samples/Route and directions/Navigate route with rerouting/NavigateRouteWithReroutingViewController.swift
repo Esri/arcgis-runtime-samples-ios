5806bd0af7f38a7908746ee23b41c66f4fcfdf12
Vivian Quach <vquach@esri.com>
2020-10-14 18:06:18 -0700

    Added files and info to content display

---
+++ arcgis-ios-sdk-samples/Route and directions/Navigate route with rerouting/NavigateRouteWithReroutingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NavigateRouteWithReroutingViewController.swift
+//  ArcGIS Runtime SDK Samples
+//
+//  Created by Vivian Quach on 10/14/20.
+//  Copyright © 2020 Esri. All rights reserved.
+//
+
+import Foundation

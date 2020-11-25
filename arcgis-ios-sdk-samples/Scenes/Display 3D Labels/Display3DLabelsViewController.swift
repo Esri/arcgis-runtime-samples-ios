5491f708ab40e821f32f06a230caca6176825fb0
Vivian Quach <vquach@esri.com>
2020-11-25 11:24:22 -0800

    Made hydrants layer

--- arcgis-ios-sdk-samples/Scenes/Display 3D Labels/Display3DLabelsViewController.swift
+++ arcgis-ios-sdk-samples/Scenes/Display 3D Labels/Display3DLabelsViewController.swift
@@ -16,15 +16,35 @@ import UIKit
 import ArcGIS
 
 class Display3DLabelsViewController: UIViewController {
-    var sublayer: AGSSubtypeSublayer!
     @IBOutlet var sceneView: AGSSceneView! {
         didSet {
-            let url = URL(string: "https://arcgisruntime.maps.arcgis.com/home/item.html?id=850dfee7d30f4d9da0ebca34a533c169")!
-            sceneView.scene = AGSScene(url: url)
-            sceneView.labeling = AGSViewLabelProperties(animationEnabled: true, labelingEnabled: true)
+            // Initialize the scene with imagery basemap.
+            sceneView.scene = makeScene()
             
+            // Set initial scene viewpoint.
+            let camera = AGSCamera(latitude: 40.704202, longitude: -74.011586, altitude: 300.0, heading: 180, pitch: 80, roll: 0)
+            sceneView.setViewpointCamera(camera)
         }
     }
+    
+    var hydrantsLayer: AGSArcGISSceneLayer!
+    
+    func makeScene() -> AGSScene {
+        // Create a scene layer from buildings REST service.
+        let hydrantsURL = URL(string: "https://services2.arcgis.com/cFEFS0EWrhfDeVw9/ArcGIS/rest/services/NYC_Utilities_Water_Hydrants/SceneServer")!
+        hydrantsLayer = AGSArcGISSceneLayer(url: hydrantsURL)
+        // Create an elevation source from Terrain3D REST service.
+//        let elevationServiceURL = URL(string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!
+//        let elevationSource = AGSArcGISTiledElevationSource(url: elevationServiceURL)
+//        let surface = AGSSurface()
+//        surface.elevationSources = [elevationSource]
+        let scene = AGSScene(basemapStyle: .arcGISLightGray)
+//        scene.baseSurface = surface
+        scene.operationalLayers.add(hydrantsLayer!)
+        return scene
+        
+    }
+    
     private func makeLabelDefinition() throws -> AGSLabelDefinition {
         // Make and stylize the text symbol.
         let textSymbol = AGSTextSymbol()
@@ -60,6 +80,20 @@ class Display3DLabelsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        hydrantsLayer.load { [weak self] (error) in
+            guard let self = self else { return }
+            if let error = error {
+                self.presentAlert(error: error)
+            } else if let layer = self.hydrantsLayer {
+                do {
+                    let label = try self.makeLabelDefinition()
+                    hydrantsLayer.labe
+                } catch {
+                    self.presentAlert(error: error)
+                }
+            }
+        }
+        
         // Add the source code button item to the right of navigation bar.
         (navigationItem.rightBarButtonItem as? SourceCodeBarButtonItem)?.filenames = ["Display3DLabelsViewController"]
     }

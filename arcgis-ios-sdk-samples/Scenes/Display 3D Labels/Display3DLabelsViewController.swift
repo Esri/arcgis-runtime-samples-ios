c9a849fe7f8d4e3197c0b12fdd807c7c1f778781
Vivian Quach <vquach@esri.com>
2020-11-24 16:24:10 -0800

    Added example label definition

--- arcgis-ios-sdk-samples/Scenes/Display 3D Labels/Display3DLabelsViewController.swift
+++ arcgis-ios-sdk-samples/Scenes/Display 3D Labels/Display3DLabelsViewController.swift
@@ -16,11 +16,44 @@ import UIKit
 import ArcGIS
 
 class Display3DLabelsViewController: UIViewController {
+    var sublayer: AGSSubtypeSublayer!
     @IBOutlet var sceneView: AGSSceneView! {
         didSet {
             let url = URL(string: "https://arcgisruntime.maps.arcgis.com/home/item.html?id=850dfee7d30f4d9da0ebca34a533c169")!
             sceneView.scene = AGSScene(url: url)
+            sceneView.labeling = AGSViewLabelProperties(animationEnabled: true, labelingEnabled: true)
+            
         }
+    }
+    private func makeLabelDefinition() throws -> AGSLabelDefinition {
+        // Make and stylize the text symbol.
+        let textSymbol = AGSTextSymbol()
+        textSymbol.angle = 0
+        textSymbol.backgroundColor = .clear
+        textSymbol.outlineColor = .white
+        textSymbol.color = .blue
+        textSymbol.haloColor = .white
+        textSymbol.haloWidth = 2
+        textSymbol.horizontalAlignment = .center
+        textSymbol.verticalAlignment = .middle
+        textSymbol.isKerningEnabled = false
+        textSymbol.offsetX = 0
+        textSymbol.offsetY = 0
+        textSymbol.fontDecoration = .none
+        textSymbol.size = 10.5
+        textSymbol.fontStyle = .normal
+        textSymbol.fontWeight = .normal
+        let textSymbolJSON = try textSymbol.toJSON()
+
+        // Make a JSON object.
+        let labelJSONObject: [String: Any] = [
+            "labelPlacement": "esriServerPointLabelPlacementAboveRight",
+            "useCodedValues": true,
+            "symbol": textSymbolJSON
+        ]
+        
+        let result = try AGSLabelDefinition.fromJSON(labelJSONObject)
+        return result as! AGSLabelDefinition
     }
     
     // MARK: UIViewController

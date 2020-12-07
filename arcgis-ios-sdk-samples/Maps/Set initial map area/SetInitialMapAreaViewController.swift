4a9b4193d6e3cb5b363942da155310cdb96f8f79
Ting Chen <tchen@esri.com>
2020-12-07 11:57:17 -0800

    Add incremental changes.

--- arcgis-ios-sdk-samples/Maps/Set initial map area/SetInitialMapAreaViewController.swift
+++ arcgis-ios-sdk-samples/Maps/Set initial map area/SetInitialMapAreaViewController.swift
@@ -17,22 +17,21 @@ import ArcGIS
 
 class SetInitialMapAreaViewController: UIViewController {
     @IBOutlet weak var mapView: AGSMapView!
-    var map: AGSMap!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //add the source code button item to the right of navigation bar
+        // Add the source code button item to the right of navigation bar.
         (self.navigationItem.rightBarButtonItem as! SourceCodeBarButtonItem).filenames = ["SetInitialMapAreaViewController"]
         
-        //initialize the map with imagery basemap
-        self.map = AGSMap(basemapStyle: .arcGISImageryStandard)
-        
-        //set initial map area
-        let envelope = AGSEnvelope(xMin: -12211308.778729, yMin: 4645116.003309, xMax: -12208257.879667, yMax: 4650542.535773, spatialReference: .webMercator())
-        self.map.initialViewpoint = AGSViewpoint(targetExtent: envelope)
+        // Initialize the map with imagery basemap.
+        let map = AGSMap(basemapStyle: .arcGISImageryStandard)
 
-        //assign the map to the map view
-        self.mapView.map = map
+        // Assign the map to the map view.
+        mapView.map = map
+        
+        // Set initial map area.
+        let envelope = AGSEnvelope(xMin: -12211308.778729, yMin: 4645116.003309, xMax: -12208257.879667, yMax: 4650542.535773, spatialReference: .webMercator())
+        mapView.setViewpoint(AGSViewpoint(targetExtent: envelope))
     }
 }

9b3b7f55c80b0216b2fc83e8d5d3211809c23b86
Ting Chen <tchen@esri.com>
2021-02-24 14:37:32 -0800

    Update viewshed camera.

--- arcgis-ios-sdk-samples/Analysis/Viewshed (camera)/ViewshedCameraViewController.swift
+++ arcgis-ios-sdk-samples/Analysis/Viewshed (camera)/ViewshedCameraViewController.swift
@@ -23,49 +23,50 @@ class ViewshedCameraViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // add the source code button item to the right of navigation bar
+        // Add the source code button item to the right of navigation bar.
         (self.navigationItem.rightBarButtonItem as! SourceCodeBarButtonItem).filenames = ["ViewshedCameraViewController"]
         
-        // initialize the scene with an imagery basemap
+        // Initialize the scene with an imagery basemap.
         let scene = AGSScene(basemap: .imagery())
         
-        // assign the scene to the scene view
+        // Assign the scene to the scene view.
         sceneView.scene = scene
         
-        // initialize the camera and set the viewpoint specified by the camera position
-        let camera = AGSCamera(location: AGSPoint(x: -4.49492, y: 48.3808, z: 48.2511, spatialReference: .wgs84()), heading: 344.488, pitch: 74.1212, roll: 0)
+        // Initialize the camera and set the viewpoint specified by the camera position.
+        let camera = AGSCamera(location: AGSPoint(x: 2.82691, y: 41.985, z: 124.987, spatialReference: .wgs84()), heading: 332.131, pitch: 82.4732, roll: 0)
         sceneView.setViewpointCamera(camera)
         
-        /// The url of the image service for elevation in Brest, France.
-        let brestElevationServiceURL = URL(string: "https://scene.arcgis.com/arcgis/rest/services/BREST_DTM_1M/ImageServer")!
-        // initialize the elevation source with the elevation service URL
-        let elevationSrc = AGSArcGISTiledElevationSource(url: brestElevationServiceURL)
+        // The URL of the world elevation image service.
+        let worldElevationServiceURL = URL(string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!
+        // Initialize the elevation source with the elevation service URL.
+        let elevationSource = AGSArcGISTiledElevationSource(url: worldElevationServiceURL)
         
-        // add the elevation source to the base surface of the scene
-        scene.baseSurface?.elevationSources.append(elevationSrc)
+        // Add the elevation source to the base surface of the scene.
+        scene.baseSurface?.elevationSources.append(elevationSource)
         
-        /// The url of the scene service for buildings in Brest, France.
-        let brestBuildingsServiceURL = URL(string: "https://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/Buildings_Brest/SceneServer/layers/0")!
-        // initialize the scene layer with the scene layer URL and add it to the scene
-        let buildings = AGSArcGISSceneLayer(url: brestBuildingsServiceURL)
+        // The URL of the scene service for buildings.
+        let gironaBuildingsServiceURL = URL(string: "https://tiles.arcgis.com/tiles/z2tnIkrLQ2BRzr6P/arcgis/rest/services/Girona_Spain/SceneServer")!
+        // Initialize the integrated mesh layer with the URL and add it to the scene.
+        let buildings = AGSIntegratedMeshLayer(url: gironaBuildingsServiceURL)
         scene.operationalLayers.add(buildings)
         
-        // create a viewshed from the camera with minimum and maximum distance (in meters) from the observer (camera) at which visibility will be evaluated
+        // Create a viewshed from the camera with minimum and maximum distance
+        // (in meters) from the observer (camera) at which visibility will be evaluated.
         let viewshed = AGSLocationViewshed(camera: camera, minDistance: 1.0, maxDistance: 500.0)
         
-        // create an analysis overlay for the viewshed and to add it to the scene view
+        // Create an analysis overlay for the viewshed and to add it to the scene view.
         let analysisOverlay = AGSAnalysisOverlay()
         analysisOverlay.analyses.add(viewshed)
         sceneView.analysisOverlays.add(analysisOverlay)
         
-        // store the viewshed for later use
+        // Store the viewshed for later use.
         self.viewshed = viewshed
     }
     
     // MARK: - Actions
 
     @IBAction func updateViewshed(_ sender: Any) {
-        // update the viewshed with the current camera
+        // Update the viewshed with the current camera.
         viewshed.update(from: sceneView.currentViewpointCamera())
     }
 }

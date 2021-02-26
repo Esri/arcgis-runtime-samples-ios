ac48e6342d7a77fb3d40a75946198f71bc653b0f
Ting Chen <tchen@esri.com>
2021-02-26 15:49:10 -0800

    Update src to address the comments.

--- arcgis-ios-sdk-samples/Display information/Create symbol styles from web styles/CreateSymbolStylesFromWebStylesViewController.swift
+++ arcgis-ios-sdk-samples/Display information/Create symbol styles from web styles/CreateSymbolStylesFromWebStylesViewController.swift
@@ -15,7 +15,7 @@
 import UIKit
 import ArcGIS
 
-class CreateSymbolStylesFromWebStylesViewController: UIViewController {
+class CreateSymbolStylesFromWebStylesViewController: UIViewController, UIAdaptivePresentationControllerDelegate {
     // MARK: Storyboard views
     
     /// The map view managed by the view controller.
@@ -38,10 +38,10 @@ class CreateSymbolStylesFromWebStylesViewController: UIViewController {
     
     /// The observation on the map view's `mapScale` property.
     var mapScaleObservation: NSKeyValueObservation?
-    /// The symbols created from the symbol style to render the feature layer.
-    var pointSymbols = [(category: SymbolCategory, symbol: AGSSymbol)]()
     /// The Esri 2D point symbol style created from a web style.
     let symbolStyle = AGSSymbolStyle(styleName: "Esri2DPointSymbolsStyle", portal: .arcGISOnline(withLoginRequired: false))
+    /// The data source that handles symbols retrieval and for the legend table display.
+    private let symbolsDataSource = SymbolsDataSource()
     
     /// A feature layer with LA County Points of Interest service.
     let featureLayer: AGSFeatureLayer = {
@@ -62,65 +62,17 @@ class CreateSymbolStylesFromWebStylesViewController: UIViewController {
         return map
     }
     
-    /// Create an `AGSUniqueValueRenderer` to rendera feature layer with symbol styles.
-    /// - Parameters:
-    ///   - fieldNames: The attributes to match the unique values against.
-    ///   - symbols: The categorized symbols to create unique values.
-    /// - Returns: An `AGSUniqueValueRenderer` object.
-    func makeUniqueValueRenderer(fieldNames: [String], symbols: [(SymbolCategory, AGSSymbol)]) -> AGSUniqueValueRenderer {
-        let renderer = AGSUniqueValueRenderer()
-        renderer.fieldNames = fieldNames
-        renderer.uniqueValues = symbols.flatMap { category, symbol in
-            // For each category value of a symbol, we need to create a
-            // unique value for it, so the field name matches to all categories.
-            category.symbolCategoryValues.map { symbolValue in
-                AGSUniqueValue(description: "", label: category.symbolName, symbol: symbol, values: [symbolValue])
-            }
-        }
-        return renderer
-    }
-    
-    /// Get certain categories of symbols from a symbol style.
-    /// - Parameters:
-    ///   - symbolStyle: An `AGSSymbolStyle` object.
-    ///   - categories: The types of symbols to search in the symbol style.
-    ///   - completion: A closure that takes an array of categorized symbols.
-    func getSymbols(symbolStyle: AGSSymbolStyle, categories: [SymbolCategory], completion: @escaping ([(category: SymbolCategory, symbol: AGSSymbol)]) -> Void) {
-        let getSymbolsGroup = DispatchGroup()
-        var symbols = [(SymbolCategory, AGSSymbol)]()
-        
-        categories.forEach { category in
-            getSymbolsGroup.enter()
-            symbolStyle.symbol(forKeys: [category.symbolName]) { symbol, _ in
-                defer {
-                    getSymbolsGroup.leave()
-                }
-                // Add the symbol to the result collection and ignore any error.
-                if let symbol = symbol {
-                    symbols.append((category, symbol))
-                }
-            }
-        }
-        getSymbolsGroup.notify(queue: .main) {
-            completion(symbols)
-        }
-    }
-    
     // MARK: UIViewController
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Add the source code button item to the right of navigation bar.
-        (navigationItem.rightBarButtonItem as? SourceCodeBarButtonItem)?.filenames = [
-            "CreateSymbolStylesFromWebStylesViewController",
-            "CreateSymbolStylesFromWebStylesLegendTableViewController"
-        ]
+        (navigationItem.rightBarButtonItem as? SourceCodeBarButtonItem)?.filenames = ["CreateSymbolStylesFromWebStylesViewController"]
         
         // Get the symbols from the symbol style hosted on an ArcGIS portal.
-        getSymbols(symbolStyle: symbolStyle, categories: SymbolCategory.allCases) { [weak self] symbols in
+        symbolsDataSource.getSymbols(symbolStyle: symbolStyle, categories: SymbolCategory.allCases) { [weak self] in
             guard let self = self else { return }
-            self.pointSymbols = symbols
-            self.featureLayer.renderer = self.makeUniqueValueRenderer(fieldNames: ["cat2"], symbols: symbols)
+            self.featureLayer.renderer = self.symbolsDataSource.makeUniqueValueRenderer(fieldNames: ["cat2"])
             self.legendBarButtonItem.isEnabled = true
         }
     }
@@ -142,85 +94,203 @@ class CreateSymbolStylesFromWebStylesViewController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "LegendTableSegue",
-           let controller = segue.destination as? CreateSymbolStylesFromWebStylesLegendTableViewController {
+           let controller = segue.destination as? UITableViewController {
             controller.presentationController?.delegate = self
-            controller.symbols = pointSymbols.map { ($0.category.symbolName, $0.symbol) }
+            controller.tableView.dataSource = symbolsDataSource
+            // Hold a weak reference to the table view to asynchronously reload
+            // image swatches for the symbols.
+            symbolsDataSource.tableView = controller.tableView
         }
     }
-}
-
-// MARK: - UIAdaptivePresentationControllerDelegate
-
-extension CreateSymbolStylesFromWebStylesViewController: UIAdaptivePresentationControllerDelegate {
+    
     func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+        // Ensure that the settings are shown in a popover on small displays.
         return .none
     }
 }
 
-extension CreateSymbolStylesFromWebStylesViewController {
-    enum SymbolCategory: CaseIterable {
-        case atm, beach, campground, cityHall, hospital, library, park, placeOfWorship, policeStation, postOffice, school, trail
-        /// The names of the symbols in the web style.
-        var symbolName: String {
-            let name: String
-            switch self {
-            case .atm:
-                name = "atm"
-            case .beach:
-                name = "beach"
-            case .campground:
-                name = "campground"
-            case .cityHall:
-                name = "city-hall"
-            case .hospital:
-                name = "hospital"
-            case .library:
-                name = "library"
-            case .park:
-                name = "park"
-            case .placeOfWorship:
-                name = "place-of-worship"
-            case .policeStation:
-                name = "police-station"
-            case .postOffice:
-                name = "post-office"
-            case .school:
-                name = "school"
-            case .trail:
-                name = "trail"
-            }
-            return name
+// MARK: - SymbolsDataSource
+
+private class SymbolsDataSource: NSObject {
+    /// A weak reference to the table view which the data source itself connects.
+    weak var tableView: UITableView?
+    
+    /// The symbols created from a symbol style.
+    private var symbols = [(category: SymbolCategory, symbol: AGSSymbol)]() {
+        didSet {
+            // Keep the array sorted whenever it is modified.
+            symbols.sort { $0.category < $1.category }
         }
-        /// The category names of features represented by a type of symbol.
-        var symbolCategoryValues: [String] {
-            let values: [String]
-            switch self {
-            case .atm:
-                values = ["Banking and Finance"]
-            case .beach:
-                values = ["Beaches and Marinas"]
-            case .campground:
-                values = ["Campgrounds"]
-            case .cityHall:
-                values = ["City Halls", "Government Offices"]
-            case .hospital:
-                values = ["Hospitals and Medical Centers", "Health Screening and Testing", "Health Centers", "Mental Health Centers"]
-            case .library:
-                values = ["Libraries"]
-            case .park:
-                values = ["Parks and Gardens"]
-            case .placeOfWorship:
-                values = ["Churches"]
-            case .policeStation:
-                values = ["Sheriff and Police Stations"]
-            case .postOffice:
-                values = ["DHL Locations", "Federal Express Locations"]
-            case .school:
-                values = ["Public High Schools", "Public Elementary Schools", "Private and Charter Schools"]
-            case .trail:
-                values = ["Trails"]
+    }
+    
+    /// A cache for the image swatches of the symbols.
+    private var cachedImages = [IndexPath: UIImage]()
+    /// A cache for cancelable operations.
+    private var imageOperations = [IndexPath: AGSCancelable]()
+    
+    /// Get certain categories of symbols from a symbol style.
+    /// - Parameters:
+    ///   - symbolStyle: An `AGSSymbolStyle` object.
+    ///   - categories: The types of symbols to search in the symbol style.
+    ///   - completion: A closure executed upon success.
+    func getSymbols(symbolStyle: AGSSymbolStyle, categories: [SymbolCategory], completion: @escaping () -> Void) {
+        let getSymbolsGroup = DispatchGroup()
+        var symbols = [(SymbolCategory, AGSSymbol)]()
+        
+        categories.forEach { category in
+            getSymbolsGroup.enter()
+            symbolStyle.symbol(forKeys: [category.symbolName]) { symbol, _ in
+                defer {
+                    getSymbolsGroup.leave()
+                }
+                // Add the symbol to the result collection and ignore any error.
+                if let symbol = symbol {
+                    symbols.append((category, symbol))
+                }
             }
-            return values
         }
+        getSymbolsGroup.notify(queue: .main) { [weak self] in
+            self?.symbols = symbols
+            completion()
+        }
+    }
+    
+    /// Create an `AGSUniqueValueRenderer` to render feature layer with symbol styles.
+    /// - Parameter fieldNames: The attributes to match the unique values against.
+    /// - Returns: An `AGSUniqueValueRenderer` object.
+    func makeUniqueValueRenderer(fieldNames: [String]) -> AGSUniqueValueRenderer {
+        let renderer = AGSUniqueValueRenderer()
+        renderer.fieldNames = fieldNames
+        renderer.uniqueValues = symbols.flatMap { category, symbol in
+            // For each category value of a symbol, we need to create a
+            // unique value for it, so the field name matches to all categories.
+            category.symbolCategoryValues.map { symbolValue in
+                AGSUniqueValue(description: "", label: category.symbolName, symbol: symbol, values: [symbolValue])
+            }
+        }
+        return renderer
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension SymbolsDataSource: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        symbols.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create a cell with basic style.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "basic", for: indexPath)
+        // Display the name of the symbol.
+        cell.textLabel?.text = symbols[indexPath.row].category.symbolName
+        // Create an image swatch for the symbol and update the cell.
+        cell.imageView?.image = createImageForRow(at: indexPath)
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        "Symbol Styles"
+    }
+    
+    /// Asynchronously create image swatches for a symbol at certain index path.
+    /// - Parameter indexPath: An index path locating a row in tableView.
+    /// - Returns: A `UIImage` if cache hits, or `nil` if the operation hasn't been finished.
+    private func createImageForRow(at indexPath: IndexPath) -> UIImage? {
+        let symbol = symbols[indexPath.row].symbol
+        if let image = cachedImages[indexPath] {
+            return image
+        } else if imageOperations[indexPath] != nil {
+            return nil
+        } else {
+            let createSwatchOperation = symbol.createSwatch { [weak self] (image, _) in
+                guard let self = self else { return }
+                self.imageOperations[indexPath] = nil
+                if let image = image {
+                    self.cachedImages[indexPath] = image
+                    self.tableView?.reloadRows(at: [indexPath], with: .automatic)
+                }
+            }
+            imageOperations[indexPath] = createSwatchOperation
+            return nil
+        }
+    }
+}
+
+// MARK: - SymbolCategory
+
+private enum SymbolCategory: CaseIterable, Comparable {
+    case atm, beach, campground, cityHall, hospital, library, park, placeOfWorship, policeStation, postOffice, school, trail
+    
+    /// The names of the symbols in the web style.
+    var symbolName: String {
+        let name: String
+        switch self {
+        case .atm:
+            name = "atm"
+        case .beach:
+            name = "beach"
+        case .campground:
+            name = "campground"
+        case .cityHall:
+            name = "city-hall"
+        case .hospital:
+            name = "hospital"
+        case .library:
+            name = "library"
+        case .park:
+            name = "park"
+        case .placeOfWorship:
+            name = "place-of-worship"
+        case .policeStation:
+            name = "police-station"
+        case .postOffice:
+            name = "post-office"
+        case .school:
+            name = "school"
+        case .trail:
+            name = "trail"
+        }
+        return name
+    }
+    
+    /// The category names of features represented by a type of symbol.
+    var symbolCategoryValues: [String] {
+        let values: [String]
+        switch self {
+        case .atm:
+            values = ["Banking and Finance"]
+        case .beach:
+            values = ["Beaches and Marinas"]
+        case .campground:
+            values = ["Campgrounds"]
+        case .cityHall:
+            values = ["City Halls", "Government Offices"]
+        case .hospital:
+            values = ["Hospitals and Medical Centers", "Health Screening and Testing", "Health Centers", "Mental Health Centers"]
+        case .library:
+            values = ["Libraries"]
+        case .park:
+            values = ["Parks and Gardens"]
+        case .placeOfWorship:
+            values = ["Churches"]
+        case .policeStation:
+            values = ["Sheriff and Police Stations"]
+        case .postOffice:
+            values = ["DHL Locations", "Federal Express Locations"]
+        case .school:
+            values = ["Public High Schools", "Public Elementary Schools", "Private and Charter Schools"]
+        case .trail:
+            values = ["Trails"]
+        }
+        return values
+    }
+    
+    static func < (lhs: SymbolCategory, rhs: SymbolCategory) -> Bool {
+        return lhs.symbolName < rhs.symbolName
     }
 }

367ae8dbeeea2a3c8239fcd8b9fe0b36eadac6f2
Ting Chen <tchen@esri.com>
2021-05-11 16:26:47 -0700

    Add further src edits, mostly working.

--- arcgis-ios-sdk-samples/Utility network/Perform valve isolation trace/PerformValveIsolationTraceViewController.swift
+++ arcgis-ios-sdk-samples/Utility network/Perform valve isolation trace/PerformValveIsolationTraceViewController.swift
@@ -43,13 +43,18 @@ class PerformValveIsolationTraceViewController: UIViewController {
     // MARK: Constant
     
     /// The URL to the feature service for running the isolation trace.
-    static let featureServiceURL = URL(string: "https://sampleserver7.arcgisonline.com/server/rest/services/UtilityNetwork/NapervilleGas/FeatureServer")!
+    static let featureServiceURL = URL(
+        string: "https://sampleserver7.arcgisonline.com/server/rest/services/UtilityNetwork/NapervilleGas/FeatureServer"
+    )!
+    
+    static let filterBarrierIdentifier = "filter barrier"
     
     // MARK: Instance properties
     
     let utilityNetwork = AGSUtilityNetwork(url: featureServiceURL)
     let serviceGeodatabase = AGSServiceGeodatabase(url: featureServiceURL)
     var traceCompleted = false
+    var identifyAction: AGSCancelable?
     
     /// The base trace parameters.
     let traceParameters = AGSUtilityTraceParameters(traceType: .isolation, startingLocations: [])
@@ -70,7 +75,7 @@ class PerformValveIsolationTraceViewController: UIViewController {
             description: "Filter Barrier",
             label: "Filter Barrier",
             symbol: barrierPointSymbol,
-            values: ["filter barrier"]
+            values: [filterBarrierIdentifier]
         )
         let startingPointSymbol = AGSSimpleMarkerSymbol(style: .cross, color: .green, size: 20)
         let renderer = AGSUniqueValueRenderer(
@@ -97,7 +102,8 @@ class PerformValveIsolationTraceViewController: UIViewController {
     /// Load the service geodatabase and initialize the layers.
     func loadServiceGeodatabase() {
         UIApplication.shared.showProgressHUD(message: "Loading service geodatabase…")
-        // NOTE: Never hardcode login information in a production application. This is done solely for the sake of the sample.
+        // NOTE: Never hardcode login information in a production application.
+        // This is done solely for the sake of the sample.
         serviceGeodatabase.credential = AGSCredential(user: "viewer01", password: "I68VGU^nMurF")
         serviceGeodatabase.load { [weak self] error in
             guard let self = self else { return }
@@ -133,7 +139,7 @@ class PerformValveIsolationTraceViewController: UIViewController {
             // Create a default starting location.
             guard let startingLocation = self.makeStartingLocation() else {
                 UIApplication.shared.hideProgressHUD()
-                self.presentAlert(message: "Fail to create starting location.")
+                self.presentAlert(message: "Failed to create starting location.")
                 self.setStatus(message: errorMessage)
                 return
             }
@@ -158,18 +164,22 @@ class PerformValveIsolationTraceViewController: UIViewController {
                 self.startingLocationPoint = startingLocationPoint
                 self.addGraphic(for: startingLocationPoint, traceLocationType: "starting point")
                 self.mapView.setViewpointCenter(startingLocationPoint, scale: 3000, completion: nil)
+                self.setStatus(
+                    message: """
+                    Utility network loaded.
+                    Tap on the map to add filter barriers, or run the trace directly without filter barriers.
+                    """
+                )
             } else if let error = error {
                 self.presentAlert(error: error)
                 self.setStatus(message: "Failed to load starting location features.")
             }
         }
         // Get available utility categories.
-        let networkDefinition = self.utilityNetwork.definition
-        self.filterBarrierCategories = networkDefinition.categories
-        self.setStatus(message: "Utility network loaded.")
-        self.categoryBarButtonItem.isEnabled = true
+        filterBarrierCategories = utilityNetwork.definition.categories
+        categoryBarButtonItem.isEnabled = true
         // Enable touch event detection on the map view.
-        self.mapView.touchDelegate = self
+        mapView.touchDelegate = self
     }
     
     // MARK: Factory methods
@@ -197,7 +207,7 @@ class PerformValveIsolationTraceViewController: UIViewController {
     }
     
     /// Get the utility tier's trace configuration and apply category comparison.
-    func makeTraceConfiguration(category: AGSUtilityCategory) -> AGSUtilityTraceConfiguration? {
+    func makeTraceConfiguration(category: AGSUtilityCategory?) -> AGSUtilityTraceConfiguration? {
         // Get a default trace configuration from a tier in the network.
         guard let configuration = utilityNetwork
                 .definition
@@ -207,15 +217,18 @@ class PerformValveIsolationTraceViewController: UIViewController {
         else {
             return nil
         }
-        // Note: `AGSUtilityNetworkAttributeComparison` or `AGSUtilityCategoryComparison`
-        // with `AGSUtilityCategoryComparisonOperator.doesNotExist` can also be used.
-        // These conditions can be joined with either `AGSUtilityTraceOrCondition`
-        // or `AGSUtilityTraceAndCondition`.
-        let comparison = AGSUtilityCategoryComparison(category: category, comparisonOperator: .exists)
-        // Create a trace filter.
-        let filter = AGSUtilityTraceFilter()
-        filter.barriers = comparison
-        configuration.filter = filter
+        if let category = category {
+            // Note: `AGSUtilityNetworkAttributeComparison` or `AGSUtilityCategoryComparison`
+            // with `AGSUtilityCategoryComparisonOperator.doesNotExist` can also be used.
+            // These conditions can be joined with either `AGSUtilityTraceOrCondition`
+            // or `AGSUtilityTraceAndCondition`.
+            // See more in the README.
+            let comparison = AGSUtilityCategoryComparison(category: category, comparisonOperator: .exists)
+            // Create a trace filter.
+            let filter = AGSUtilityTraceFilter()
+            filter.barriers = comparison
+            configuration.filter = filter
+        }
         configuration.includeIsolatedFeatures = isolationSwitch.isOn
         return configuration
     }
@@ -268,25 +281,28 @@ class PerformValveIsolationTraceViewController: UIViewController {
     // MARK: Actions
     
     func trace(completion: @escaping () -> Void) {
-        guard let configuration = makeTraceConfiguration(category: selectedCategory!) else {
-            setStatus(message: "Fail to get trace configuration.")
+        guard let configuration = makeTraceConfiguration(category: selectedCategory) else {
+            setStatus(message: "Failed to get trace configuration.")
             return
         }
         traceParameters.traceConfiguration = configuration
         
         utilityNetwork.trace(with: traceParameters) { [weak self] (traceResults, error) in
             guard let self = self else { return }
-            let categoryName = self.selectedCategory!.name.lowercased()
             if let elementTraceResult = traceResults?.first as? AGSUtilityElementTraceResult,
                !elementTraceResult.elements.isEmpty {
                 self.selectFeatures(in: elementTraceResult.elements) {
-                    self.setStatus(message: "Trace with \(categoryName) category completed.")
+                    if let categoryName = self.selectedCategory?.name.lowercased() {
+                        self.setStatus(message: "Trace with \(categoryName) category completed.")
+                    } else {
+                        self.setStatus(message: "Trace with filter barriers completed.")
+                    }
                 }
             } else if let error = error {
-                self.setStatus(message: "Trace with \(categoryName) category failed.")
+                self.setStatus(message: "Trace failed.")
                 self.presentAlert(error: error)
             } else {
-                self.setStatus(message: "Trace with \(categoryName) category completed with no output.")
+                self.setStatus(message: "Trace completed with no output.")
             }
             completion()
         }
@@ -297,18 +313,19 @@ class PerformValveIsolationTraceViewController: UIViewController {
         case true:
             // Reset the trace if it is already completed
             clearLayersSelection()
+            traceParameters.filterBarriers.removeAll()
             parametersOverlay.graphics.removeAllObjects()
+            traceCompleted = false
+            selectedCategory = nil
             // Add back the starting location.
-            // Get the geometry of the first feature for the starting location as a point.
             addGraphic(for: startingLocationPoint, traceLocationType: "starting point")
             mapView.setViewpointCenter(startingLocationPoint, scale: 3000, completion: nil)
-            setStatus(message: "Instructions shown here.")
+            // Set UI state.
+            setStatus(message: "Tap on the map to add filter barriers, or run the trace directly without filter barriers.")
             traceResetBarButtonItem.title = "Trace"
             traceResetBarButtonItem.isEnabled = false
             categoryBarButtonItem.isEnabled = true
             isolationSwitch.isEnabled = true
-            traceCompleted = false
-            selectedCategory = nil
         case false:
             UIApplication.shared.showProgressHUD(message: "Running isolation trace…")
             // Run the trace.
@@ -355,8 +372,101 @@ class PerformValveIsolationTraceViewController: UIViewController {
     }
 }
 
+// MARK: - AGSGeoViewTouchDelegate
+
 extension PerformValveIsolationTraceViewController: AGSGeoViewTouchDelegate {
     func geoView(_ geoView: AGSGeoView, didTapAtScreenPoint screenPoint: CGPoint, mapPoint: AGSPoint) {
+        if let identifyAction = identifyAction {
+            identifyAction.cancel()
+        }
+        identifyAction = mapView.identifyLayers(atScreenPoint: screenPoint, tolerance: 10, returnPopupsOnly: false) { [weak self] result, error in
+            guard let self = self else { return }
+            if let feature = result?.first?.geoElements.first as? AGSArcGISFeature {
+                self.addFilterBarrier(for: feature, at: mapPoint)
+            } else if let error = error {
+                self.setStatus(message: "Error identifying trace locations.")
+                self.presentAlert(error: error)
+            }
+        }
+    }
+    
+    /// Add a graphic at the tapped location for the filter barrier.
+    /// - Parameters:
+    ///   - feature: The geoelement retrieved as an `AGSFeature`.
+    ///   - location: The `AGSPoint` used to identify utility elements in the utility network.
+    func addFilterBarrier(for feature: AGSArcGISFeature, at location: AGSPoint) {
+        guard let geometry = feature.geometry,
+              let element = utilityNetwork.createElement(with: feature) else {
+            return
+        }
+        let elementDidSet = { [weak self] in
+            guard let self = self else { return }
+            if self.categoryBarButtonItem.isEnabled {
+                self.categoryBarButtonItem.isEnabled = false
+                self.selectedCategory = nil
+            }
+            if !self.traceResetBarButtonItem.isEnabled {
+                self.traceResetBarButtonItem.isEnabled = true
+            }
+            self.traceParameters.filterBarriers.append(element)
+            let point = geometry as? AGSPoint ?? location
+            self.addGraphic(for: point, traceLocationType: PerformValveIsolationTraceViewController.filterBarrierIdentifier)
+        }
         
+        switch element.networkSource.sourceType {
+        case .junction:
+            // If the user tapped on a junction, get the asset's terminal(s).
+            if let terminals = element.assetType.terminalConfiguration?.terminals {
+                selectTerminal(from: terminals, at: location) { [weak self] terminal in
+                    guard let self = self else { return }
+                    element.terminal = terminal
+                    elementDidSet()
+                    self.setStatus(message: String(format: "Juntion element with terminal %@ added to the filter barriers.", terminal.name))
+                }
+            }
+        case .edge:
+            // If the user tapped on an edge, determine how far along that edge.
+            if let line = AGSGeometryEngine.geometryByRemovingZ(from: geometry) as? AGSPolyline {
+                element.fractionAlongEdge = AGSGeometryEngine.fraction(alongLine: line, to: location, tolerance: -1)
+                elementDidSet()
+                setStatus(message: String(format: "Edge element at fractionAlongEdge %.3f added to the filter barriers.", element.fractionAlongEdge))
+            }
+        @unknown default:
+            return
+        }
+    }
+    
+    /// Presents an action sheet to select one from multiple terminals, or return if there is only one.
+    /// - Parameters:
+    ///   - terminals: An array of terminals.
+    ///   - mapPoint: The location tapped on the map.
+    ///   - completion: Completion closure to pass the selected terminal.
+    func selectTerminal(from terminals: [AGSUtilityTerminal], at mapPoint: AGSPoint, completion: @escaping (AGSUtilityTerminal) -> Void) {
+        if terminals.count > 1 {
+            // Show a terminal picker
+            let terminalPicker = UIAlertController(
+                title: "Select a terminal.",
+                message: nil,
+                preferredStyle: .actionSheet
+            )
+            terminals.forEach { terminal in
+                let action = UIAlertAction(title: terminal.name, style: .default) { [terminal] _ in
+                    completion(terminal)
+                }
+                terminalPicker.addAction(action)
+            }
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+            terminalPicker.addAction(cancelAction)
+            if let popoverController = terminalPicker.popoverPresentationController {
+                // If presenting in a split view controller (e.g. on an iPad),
+                // provide positioning information for the alert controller.
+                popoverController.sourceView = mapView
+                let tapPoint = mapView.location(toScreen: mapPoint)
+                popoverController.sourceRect = CGRect(origin: tapPoint, size: .zero)
+            }
+            present(terminalPicker, animated: true, completion: nil)
+        } else if let terminal = terminals.first {
+            completion(terminal)
+        }
     }
 }

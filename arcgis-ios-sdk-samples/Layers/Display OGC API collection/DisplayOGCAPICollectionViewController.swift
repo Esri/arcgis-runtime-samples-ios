304f9262a64f1223bcd1e28cea44ea20ae8144f0
Ting Chen <tchen@esri.com>
2021-05-20 16:29:30 -0700

    Add source code and UI for the sample.

---
+++ arcgis-ios-sdk-samples/Layers/Display OGC API collection/DisplayOGCAPICollectionViewController.swift
@@ -0,0 +1,102 @@
+// Copyright 2021 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import UIKit
+import ArcGIS
+
+class DisplayOGCAPICollectionViewController: UIViewController {
+    /// The map view managed by the view controller.
+    @IBOutlet weak var mapView: AGSMapView! {
+        didSet {
+            let map = AGSMap(basemapStyle: .arcGISTopographic)
+            map.operationalLayers.add(ogcFeatureLayer)
+            mapView.map = map
+            // Set the viewpoint to Daraa, Syria.
+            mapView.setViewpoint(AGSViewpoint(latitude: 32.62, longitude: 36.10, scale: 20_000))
+        }
+    }
+    
+    // MARK: Instance properties
+    
+    /// The last query job.
+    var lastQuery: AGSCancelable?
+    /// The KVO on navigating status of the map view.
+    var navigatingObservation: NSKeyValueObservation?
+    
+    /// A feature layer to visualize the OGC API features.
+    let ogcFeatureLayer: AGSFeatureLayer = {
+        // Note: the collection ID can be accessed later via
+        // `featureCollectionInfo.collectionID` property of the feature table.
+        let table = AGSOGCFeatureCollectionTable(
+            url: URL(string: "https://demo.ldproxy.net/daraa")!,
+            collectionID: "TransportationGroundCrv"
+        )
+        // Set the feature request mode to manual (only manual is currently
+        // supported). In this mode, you must manually populate the table -
+        // panning and zooming won't request features automatically.
+        table.featureRequestMode = .manualCache
+        
+        let featureLayer = AGSFeatureLayer(featureTable: table)
+        let lineSymbol = AGSSimpleLineSymbol(style: .solid, color: .blue, width: 3)
+        featureLayer.renderer = AGSSimpleRenderer(symbol: lineSymbol)
+        return featureLayer
+    }()
+    
+    // MARK: Method
+    
+    func populateFeaturesFromQuery() {
+        // Cancel if there is an existing query request.
+        lastQuery?.cancel()
+        
+        // Create a query based on the current visible extent.
+        let visibleExtentQuery = AGSQueryParameters()
+        visibleExtentQuery.geometry = mapView.visibleArea?.extent
+        visibleExtentQuery.spatialRelationship = .intersects
+        // Set a limit of 5000 on the number of returned features per request,
+        // because the default on some services could be as low as 10.
+        visibleExtentQuery.maxFeatures = 5_000
+        
+        // Populate the table with the query. Setting `outFields` to `nil`
+        // requests all fields.
+        let table = ogcFeatureLayer.featureTable as! AGSOGCFeatureCollectionTable
+        lastQuery = table.populateFromService(
+            with: visibleExtentQuery,
+            clearCache: false,
+            outfields: nil
+        ) { [weak self] _, error in
+            if let error = error, (error as NSError).code != NSUserCancelledError {
+                // Do not display error if user simply cancelled the request.
+                self?.presentAlert(error: error)
+            }
+        }
+    }
+    
+    // MARK: UIViewController
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Add the source code button item to the right of navigation bar.
+        (navigationItem.rightBarButtonItem as? SourceCodeBarButtonItem)?.filenames = ["DisplayOGCAPICollectionViewController"]
+        // Query for features with the initial extent.
+        populateFeaturesFromQuery()
+        // Query for features whenever map's viewpoint change has finished.
+        navigatingObservation = mapView.observe(\.isNavigating, options: .new) { _, change in
+            if change.newValue == false {
+                DispatchQueue.main.async {
+                    self.populateFeaturesFromQuery()
+                }
+            }
+        }
+    }
+}

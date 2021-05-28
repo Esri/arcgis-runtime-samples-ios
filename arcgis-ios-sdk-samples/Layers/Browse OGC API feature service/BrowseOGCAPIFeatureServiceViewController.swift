226cf79b139e737d1f4bd0d39c2ca66417ea0087
Ting Chen <tchen@esri.com>
2021-05-28 11:19:24 -0700

    Nilify the query in completion.

--- arcgis-ios-sdk-samples/Layers/Browse OGC API feature service/BrowseOGCAPIFeatureServiceViewController.swift
+++ arcgis-ios-sdk-samples/Layers/Browse OGC API feature service/BrowseOGCAPIFeatureServiceViewController.swift
@@ -86,6 +86,7 @@ class BrowseOGCAPIFeatureServiceViewController: UIViewController {
             outfields: nil
         ) { [weak self, table] _, error in
             guard let self = self else { return }
+            self.lastQuery = nil
             if let error = error,
                // Do not display error if user simply cancelled the request.
                (error as NSError).code != NSUserCancelledError {

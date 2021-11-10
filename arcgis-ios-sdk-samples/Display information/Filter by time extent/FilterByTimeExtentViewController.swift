b6ff430df3df893c65d6f3355aee89fa2615f940
Vivian Quach <viv10382@esri.com>
2021-11-09 22:08:29 -0800

    Removed extra view and toolbar

--- arcgis-ios-sdk-samples/Display information/Filter by time extent/FilterByTimeExtentViewController.swift
+++ arcgis-ios-sdk-samples/Display information/Filter by time extent/FilterByTimeExtentViewController.swift
@@ -1,4 +1,3 @@
-
 // Copyright 2021 Esri
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,7 +17,7 @@ import ArcGIS
 import ArcGISToolkit
 
 class FilterByTimeExtentViewController: UIViewController {
-    @IBOutlet var mapView: AGSMapView!{
+    @IBOutlet var mapView: AGSMapView! {
         didSet {
             mapView.map = AGSMap(basemapStyle: .arcGISTopographic)
             let url = URL(string: "https://services5.arcgis.com/N82JbI5EYtAkuUKU/ArcGIS/rest/services/Hurricane_time_enabled_layer_2005_1_day/FeatureServer/0")
@@ -27,18 +26,32 @@ class FilterByTimeExtentViewController: UIViewController {
             mapView.map?.operationalLayers.add(featureLayer)
         }
     }
-    @IBOutlet var timeSliderView: UIView!
     
     func displayTimeSlider() {
         let timeSlider = TimeSlider()
-        timeSliderView.addSubview(timeSlider)
-        
+        timeSlider.labelMode = .thumbs
+        timeSlider.addTarget(self, action: #selector(FilterByTimeExtentViewController.timeSliderValueChanged(timeSlider:)), for: .valueChanged)
+        view.addSubview(timeSlider)
+        let sideMargin: CGFloat = 10.0
+        let bottomMargin: CGFloat = 30.0
+        timeSlider.translatesAutoresizingMaskIntoConstraints = false
+        timeSlider.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -bottomMargin).isActive = true
+        timeSlider.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: sideMargin).isActive = true
+        timeSlider.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -sideMargin).isActive = true
+    }
+    
+    @objc
+    func timeSliderValueChanged(timeSlider: TimeSlider) {
+        if mapView.timeExtent != timeSlider.currentExtent {
+            mapView.timeExtent = timeSlider.currentExtent
+        }
     }
 
     // MARK: UIViewController
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        displayTimeSlider()
         // Add the source code button item to the right of navigation bar.
         (navigationItem.rightBarButtonItem as? SourceCodeBarButtonItem)?.filenames = ["FilterByTimeExtentViewController"]
     }

51d69425f07f168142b82678212ad2429d56ef8e
Vivian Quach <viv10382@esri.com>
2021-11-11 15:15:38 -0800

    Add comments

--- arcgis-ios-sdk-samples/Display information/Filter by time extent/FilterByTimeExtentViewController.swift
+++ arcgis-ios-sdk-samples/Display information/Filter by time extent/FilterByTimeExtentViewController.swift
@@ -17,27 +17,94 @@ import ArcGIS
 import ArcGISToolkit
 
 class FilterByTimeExtentViewController: UIViewController {
+    /// The map view.
     @IBOutlet var mapView: AGSMapView! {
         didSet {
             mapView.map = AGSMap(basemapStyle: .arcGISTopographic)
-            let url = URL(string: "https://services5.arcgis.com/N82JbI5EYtAkuUKU/ArcGIS/rest/services/Hurricane_time_enabled_layer_2005_1_day/FeatureServer/0")
-            let featureTable = AGSServiceFeatureTable(url: url!)
-            let featureLayer = AGSFeatureLayer(featureTable: featureTable)
-            mapView.map?.operationalLayers.add(featureLayer)
         }
     }
     
-    func displayTimeSlider() {
-        let timeSlider = TimeSlider()
+    /// The date formatter.
+    static let formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd HH:mm"
+        return formatter
+    }()
+    
+    /// The data's start date.
+    let startTime = formatter.date(from: "2005/9/1 05:00")!
+    /// The data's end date.
+    let endTime = formatter.date(from: "2005/12/31 05:00")!
+    /// The default date and time for the starting thumb.
+    let currentStartTime = formatter.date(from: "2005/10/01 12:00")
+    /// The default date and time for the ending thumb.
+    let currentEndTime = formatter.date(from: "2005/10/31 12:00")
+    
+    /// The feature table URL tracking hurricanes in 2005.
+    static let featureTableURL = URL(string: "https://services5.arcgis.com/N82JbI5EYtAkuUKU/ArcGIS/rest/services/Hurricane_time_enabled_layer_2005_1_day/FeatureServer/0")!
+    
+    /// The feature layer made from the feature table.
+    let featureLayer = AGSFeatureLayer(
+        featureTable: AGSServiceFeatureTable(
+            url: featureTableURL
+        )
+    )
+    
+    /// The time slider from the ArcGIS toolkit.
+    let timeSlider = TimeSlider()
+    
+    /// Configure the time slider's attributes and position.
+    func setupTimeSlider() {
+        // Configure time slider.
         timeSlider.labelMode = .thumbs
-        timeSlider.addTarget(self, action: #selector(FilterByTimeExtentViewController.timeSliderValueChanged(timeSlider:)), for: .valueChanged)
+        timeSlider.addTarget(self, action: #selector(timeSliderValueChanged(timeSlider:)), for: .valueChanged)
+        // Add the time slider to the view.
         view.addSubview(timeSlider)
-        let sideMargin: CGFloat = 10.0
-        let bottomMargin: CGFloat = 30.0
+        
+        // Add constraints to position the slider.
+        let margin: CGFloat = 10.0
         timeSlider.translatesAutoresizingMaskIntoConstraints = false
-        timeSlider.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -bottomMargin).isActive = true
-        timeSlider.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: sideMargin).isActive = true
-        timeSlider.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -sideMargin).isActive = true
+        // Set the time slider on top of the attribution bar in the map view.
+        timeSlider.bottomAnchor.constraint(equalTo: mapView.attributionTopAnchor, constant: -margin).isActive = true
+        // Set the side constraints with padding for the time slider .
+        timeSlider.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: margin).isActive = true
+        timeSlider.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -margin).isActive = true
+    }
+    
+    /// Initialize the time slider's steps.
+    func initializeTimeStepsFromQuery() {
+        featureLayer.load { [unowned self] error in
+            guard error == nil else { fatalError(error!.localizedDescription) }
+            populateFeaturesWithQuery { _ in
+                timeSlider.initializeTimeSteps(timeStepCount: 200, fullExtent: AGSTimeExtent(startTime: startTime, endTime: endTime)) { _ in
+                    // Show the time slider.
+                    timeSlider.currentExtent = AGSTimeExtent(startTime: currentStartTime, endTime: currentEndTime)
+                    
+                    mapView.map!.operationalLayers.add(featureLayer)
+                }
+            }
+        }
+    }
+    
+    /// Populate the features using the requested time extent.
+    func populateFeaturesWithQuery(completion: @escaping (AGSFeatureQueryResult) -> Void) {
+        let featureTable = featureLayer.featureTable as! AGSServiceFeatureTable
+        // Create query parameters.
+        let queryParams = AGSQueryParameters()
+        // Create a new time extent that covers the desired interval.
+        let timeExtent = AGSTimeExtent(startTime: startTime, endTime: endTime)
+        
+        // Apply the time extent to query parameters to filter features based on time.
+        queryParams.timeExtent = timeExtent
+        // Set the feature request mode to load the features faster.
+        featureTable.featureRequestMode = .manualCache
+        // Populate features based on query parameters.
+        featureTable.populateFromService(with: queryParams, clearCache: true, outFields: ["*"]) { (result: AGSFeatureQueryResult?, error: Error?) in
+            guard error == nil else { return }
+            if let result = result {
+                completion(result)
+            }
+        }
     }
     
     @objc
@@ -46,12 +113,13 @@ class FilterByTimeExtentViewController: UIViewController {
             mapView.timeExtent = timeSlider.currentExtent
         }
     }
-
+    
     // MARK: UIViewController
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        displayTimeSlider()
+        setupTimeSlider()
+        initializeTimeStepsFromQuery()
         // Add the source code button item to the right of navigation bar.
         (navigationItem.rightBarButtonItem as? SourceCodeBarButtonItem)?.filenames = ["FilterByTimeExtentViewController"]
     }

ab8b3219b276d8ad050af28a735651dff0bf69a2
Vivian Quach <viv10382@esri.com>
2022-05-12 12:29:45 -0700

    Added FavoritesCategoryVC

---
+++ arcgis-ios-sdk-samples/Content Display Logic/Controllers/FavoritesCategoryViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FavoritesCategoryViewController.swift
+//  ArcGIS Runtime SDK Samples
+//
+//  Created by Vivian Quach on 5/11/22.
+//  Copyright © 2022 Esri. All rights reserved.
+//
+
+import Foundation

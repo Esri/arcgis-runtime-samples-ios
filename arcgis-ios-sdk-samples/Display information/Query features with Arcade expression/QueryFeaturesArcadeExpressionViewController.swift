60b98c8678e049f4135d32f0f60855cb749283a6
Vivian Quach <viv10382@esri.com>
2022-06-27 13:19:58 -0700

    Add comments

--- arcgis-ios-sdk-samples/Display information/Query features with Arcade expression/QueryFeaturesArcadeExpressionViewController.swift
+++ arcgis-ios-sdk-samples/Display information/Query features with Arcade expression/QueryFeaturesArcadeExpressionViewController.swift
@@ -19,18 +19,23 @@ class QueryFeaturesArcadeExpressionViewController: UIViewController {
     @IBOutlet var mapView: AGSMapView! {
         didSet {
             mapView.map = makeMap()
+            // Set the touch delegate.
             mapView.touchDelegate = self
         }
     }
     
-    var previousFeature: AGSArcGISFeature?
-    var detail: String?
+    /// The arcade expression evaluation operation.
+    var evaluateOperation: AGSCancelable?
     
+    /// Make and load a map.
     func makeMap() -> AGSMap {
-        let portal = AGSPortal.arcGISOnline(withLoginRequired: false)
-        let portalItem = AGSPortalItem(portal: portal, itemID: "14562fced3474190b52d315bc19127f6")
+        // Create a portal item with the portal and item ID.
+        let portalItem = AGSPortalItem(portal: .arcGISOnline(withLoginRequired: false), itemID: "14562fced3474190b52d315bc19127f6")
+        // Make a map with the portal item.
         let map = AGSMap(item: portalItem)
-        map.load() { error in
+        // Load the map.
+        map.load() { _ in
+            // Set the visibility of all but the RDT Beats layer to false.
             map.operationalLayers.forEach { layer in
                 let currentLayer = layer as? AGSLayer
                 if currentLayer?.name == "Crime in the last 60 days" || currentLayer?.name == "Police Stations" {
@@ -41,21 +46,33 @@ class QueryFeaturesArcadeExpressionViewController: UIViewController {
         return map
     }
     
+    /// Evaluate the arcade expression for the selected feature at the map point.
     func evaluateArcadeInCallout(for feature: AGSArcGISFeature, at mapPoint: AGSPoint) {
-        if self.previousFeature == nil {
-            let expressionValue = "var crimes = FeatureSetByName($map, 'Crime in the last 60 days');\n" + "return Count(Intersects($feature, crimes));"
-            let expression = AGSArcadeExpression(expression: expressionValue)
-            let evaluator = AGSArcadeEvaluator(expression: expression, profile: .formCalculation)
-            guard let map = mapView.map else { return }
-            let profileVariables = ["$feature": feature, "$map": map]
-            evaluator.evaluate(withProfileVariables: profileVariables) { [weak self] result, error in
-                guard let self = self else { return }
-                if let result = result, let crimeCount = result.cast(to: .string) as? String {
-                    self.detail = "Crimes in the last 60 days: \(crimeCount)"
-                    self.previousFeature = feature
-                } else if let error = error {
-                    self.presentAlert(error: error)
-                }
+        // Show progress hud.
+        UIApplication.shared.showProgressHUD(message: "Evaluating")
+        // Instantiate a string containing the arcade expression.
+        let expressionValue = "var crimes = FeatureSetByName($map, 'Crime in the last 60 days');\n" + "return Count(Intersects($feature, crimes));"
+        // Create an arcade expression using the string.
+        let expression = AGSArcadeExpression(expression: expressionValue)
+        // Create an arcade evaluator with the arcade expression and an arcade profile.
+        let evaluator = AGSArcadeEvaluator(expression: expression, profile: .formCalculation)
+        guard let map = mapView.map else { return }
+        let profileVariables = ["$feature": feature, "$map": map]
+        // Get the arcade evaluation result given the previously set profile variables.
+        evaluateOperation = evaluator.evaluate(withProfileVariables: profileVariables) { [weak self] result, error in
+            guard let self = self else { return }
+            if let result = result, let crimeCount = result.cast(to: .string) as? String {
+                // Dismiss progress hud.
+                UIApplication.shared.hideProgressHUD()
+                // Hide the accessory button.
+                self.mapView.callout.isAccessoryButtonHidden = true
+                // Set the detail text.
+                self.mapView.callout.detail = "Crimes in the last 60 days: \(crimeCount)"
+                // Prompt the callout at the map point.
+                self.mapView.callout.show(at: mapPoint, screenOffset: .zero, rotateOffsetWithMap: false, animated: true)
+            } else if let error = error {
+                // Present an error if needed.
+                self.presentAlert(error: error)
             }
         }
     }
@@ -64,7 +81,7 @@ class QueryFeaturesArcadeExpressionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Add the source code button item to the right of navigation bar.
         (navigationItem.rightBarButtonItem as? SourceCodeBarButtonItem)?.filenames = [
             "QueryFeaturesArcadeExpressionViewController"
         ]
@@ -81,12 +98,12 @@ extension QueryFeaturesArcadeExpressionViewController: AGSGeoViewTouchDelegate {
         mapView.identifyLayers(atScreenPoint: screenPoint, tolerance: 12, returnPopupsOnly: false) { [weak self] results, error in
             guard let results = results, let self = self else { return }
             if let elements = results.first?.geoElements {
+                // Get the selected feature.
                 guard let identifiedFeature = elements.first as? AGSArcGISFeature else { return }
+                // Evaluate the arcade for the given feature.
                 self.evaluateArcadeInCallout(for: identifiedFeature, at: mapPoint)
-                self.mapView.callout.isAccessoryButtonHidden = true
-                self.mapView.callout.detail = self.detail
-                self.mapView.callout.show(at: mapPoint, screenOffset: .zero, rotateOffsetWithMap: false, animated: true)
             } else if let error = error {
+                // Present an error alert if needed.
                 self.presentAlert(error: error)
             }
         }
